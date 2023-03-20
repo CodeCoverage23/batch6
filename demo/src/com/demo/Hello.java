5d136b3565d57b66ce00fb4d9c722bc6854d1a6c
mansi <mansi@ASUS>
2023-03-20 16:34:49 +0530

    Updated

--- demo/src/com/demo/Hello.java
+++ demo/src/com/demo/Hello.java
@@ -5,7 +5,8 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("hello java");
-System.out.println("hel");
+System.out.println("hello");
+System.out.println("welcome gihub");
 
 	}
 
