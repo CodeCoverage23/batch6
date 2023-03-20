5a572ecda2a88e66dd907ae53a570bfe964c4de1
Manthan Aage <Manthan Aage@LAPTOP-09BEGS6L>
2023-03-20 21:36:31 +0530

    Operator program

---
+++ demo/src/com/demo/operatorsjava.java
@@ -0,0 +1,44 @@
+package com.demo;
+
+public class operatorsjava{
+	
+	public static void main(String[] args){
+		
+		int a=10;
+		int b=20;
+		int c=30;
+		//System.out.println((a>++b)&&(++a<c));//logical AND
+		//System.out.println(a<b & ++a>b);//logical bitwise
+		//System.out.println(a<b | ++a>b);//Logical or
+		System.out.println(a<b | ++a>b);
+		System.out.println(a);
+		System.out.println(b);
+	
+		
+		
+		
+		
+		
+		
+		
+	
+		}
+
+		
+				
+				
+				
+				
+				
+				
+				
+			
+		
+		
+	
+}
+
+		
+		
+		
+	
