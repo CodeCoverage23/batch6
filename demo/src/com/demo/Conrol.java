5fd5292360a99aa8d16f5fa35310df3c3619b1d8
91883 <91883@mohan>
2023-03-22 20:24:24 +0530

    ifelse

--- demo/src/com/demo/Conrol.java
+++ demo/src/com/demo/Conrol.java
@@ -4,11 +4,15 @@ public class Conrol {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a=20;
-		if (a%2==0);
+		int a=25;
+		if (a%2==0)
+		{
 		System.out.println("Number is Even");
 		}
-			
+		else {
+			System.out.println("number is odd");
+			}
+		}
 
 	}
 
