428aa595a88985519300f2f7d1deedfe91aea189
Lenovo <Lenovo@lenovo>
2023-03-22 22:13:51 +0530

    My first commit

---
+++ Myfirstproject/src/com/demo/Hellojava.java
@@ -0,0 +1,12 @@
+package com.demo;
+
+public class Hellojava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Java");
+		System.out.println("Welcome to CodeCoverage");
+
+	}
+
+}
