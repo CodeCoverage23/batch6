408247ed63ca39fa14bdbedb064f95d8c6fdd7d6
admin <admin@DESKTOP-5S64ACI>
2023-03-22 22:05:35 +0530

    first program

---
+++ demo/src/com/demo/Demo_if_else.java
@@ -0,0 +1,19 @@
+package com.demo;
+
+public class Demo_if_else {
+	public static void main(String[]args) {
+       int age = 23;
+       {
+			
+			if(age>=18) {
+				System.out.println("Eligible for vote");
+			}
+			else {
+				System.out.println("Not eligible for vote");
+			}
+		}
+	}
+
+	}
+
+
