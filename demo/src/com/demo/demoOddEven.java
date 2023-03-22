da493dd8aa4628d6174370d0bd4e36d1dcf550fd
oavid Dubbeldan <Kvlokhande2001@gmail.com>
2023-03-22 11:08:10 -0700

    Updated

---
+++ demo/src/com/demo/demoOddEven.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+public class demoOddEven {
+	public static void main (String[]ars) {
+		int a = 35;
+		if(a % 2 ==0) {
+			System.out.println("Number is Even");
+		}else {
+			System.out.println("Number is odd");
+		}
+	}
+
+}
