3b586fedc4a59927921d54d6f0bcfc207d41a015
admin <admin@DESKTOP-5S64ACI>
2023-03-23 18:18:32 +0530

    if_else

---
+++ demo/src/com/demo/If_else.java
@@ -0,0 +1,19 @@
+package com.demo;
+
+public abstract class If_else {
+
+	public static void main(String[]args) {
+		
+             int number = 45;
+		
+		if(number%2==0) {
+			System.out.println(" Number is even");
+		}
+		else {
+			System.out.println("Number is odd");
+		}
+	}
+
+
+	}
+
