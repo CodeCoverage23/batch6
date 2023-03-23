75debfb8a62c29e480bd412ac5b0a6681f3bfc89
User <User@DESKTOP-OQ2KTH4>
2023-03-23 22:46:17 +0530

    Even and odd program

--- demo/src/com/demo/EvenOdd.java
+++ demo/src/com/demo/EvenOdd.java
@@ -1,13 +1,16 @@
 package com.demo;
+import java.util.*;
 
 public class EvenOdd {
  public static void main(String[] args) {
-	 int a=23;
+	 Scanner scan =new Scanner(System.in);
+	 System.out.println("Enter the number:");
+	 int a=scan.nextInt();
 	 if(a%2==0) {
-		 System.out.println("The number is even="+a);
+		 System.out.println("The  given number is even");
 	 }
 	 else {
-		 System.out.println("The number is odd="+a);
+		 System.out.println("The given number is odd");
 	 }
 }
 }
