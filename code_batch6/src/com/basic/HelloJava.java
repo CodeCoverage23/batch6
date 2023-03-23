60ead16dd0c22e850afd06ee5f54ca3bde21c55e
user <user@LAPTOP-9K2SO253>
2023-03-23 07:10:43 +0530

    upDate

--- code_batch6/src/com/basic/HelloJava.java
+++ code_batch6/src/com/basic/HelloJava.java
@@ -5,6 +5,7 @@ public class HelloJava {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Code C");
+		System.out.println("Hallo Java");
 
 	}
 
