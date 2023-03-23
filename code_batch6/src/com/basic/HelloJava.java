23697b6cc319623f83e790d34d97a66c1ad2d6ca
Lenovo <Lenovo@lenovo>
2023-03-23 13:13:54 +0530

    Updated

--- code_batch6/src/com/basic/HelloJava.java
+++ code_batch6/src/com/basic/HelloJava.java
@@ -4,7 +4,7 @@ public class HelloJava {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		System.out.println("Code C");
+		System.out.println("Code Coverage");
 
 	}
 
