fc787c228c2991510348e1084469d622d8f6c259
Hemraj <Hemraj@DESKTOP-U0HKUMH>
2023-03-23 17:36:15 +0530

    see

---
+++ demo/src/com/demo/IfElse.java
@@ -0,0 +1,20 @@
+package com.demo;
+
+public class IfElse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int h = 40;
+        int i = 34;
+        int j = 44;
+     
+        if(h<i&&h<j)
+        {
+        System.out.println("condition is true");
+	}
+	else {
+		System.out.println("condition is not true");
+	}
+
+}
+}
