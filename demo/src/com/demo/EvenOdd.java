eb96106951bbf5d2748ac883892e407ecb2445da
User <User@DESKTOP-OQ2KTH4>
2023-03-23 19:43:06 +0530

    my 1st commit

---
+++ demo/src/com/demo/EvenOdd.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+public class EvenOdd {
+ public static void main(String[] args) {
+	 int a=23;
+	 if(a%2==0) {
+		 System.out.println("The number is even="+a);
+	 }
+	 else {
+		 System.out.println("The number is odd="+a);
+	 }
+}
+}
