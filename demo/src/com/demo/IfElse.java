1cf7bd4fe19801a0af0d15a72a157708d34fe265
prade <prade@DESKTOP-NS5DLR9>
2023-03-23 15:39:46 +0530

    if else

---
+++ demo/src/com/demo/IfElse.java
@@ -0,0 +1,24 @@
+package com.demo;
+
+public class IfElse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a = 10;
+		int b = 20;
+		int c = 30;                                                               
+
+		if (a > b || b == c)//false||true=true;
+		{
+		
+		int d=a+b+c;
+		System.out.println(d);
+
+	}
+		else {
+			System.out.println(" condition is failed");
+			
+		}
+
+}}
