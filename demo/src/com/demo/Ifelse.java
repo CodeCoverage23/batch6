2e0fc31f708cf0d593f88b1b967cd17354633c9e
User <User@DESKTOP-OQ2KTH4>
2023-03-23 19:36:43 +0530

    my 1st commit

---
+++ demo/src/com/demo/Ifelse.java
@@ -0,0 +1,17 @@
+package com.demo;
+
+public class Ifelse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      int num=10;
+      
+      if(num%2==0) {
+    	  System.out.println("The Given number is even="+num);
+      }
+      else {
+    	  System.out.println("The given number is odd="+num);
+      }
+	}
+
+}
