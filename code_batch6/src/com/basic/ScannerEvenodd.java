52965442bab3e74ce401a5edd066991764510cc2
lenovo <lenovo@DESKTOP-PF8O34S>
2023-03-23 22:24:30 +0530

    Even odd using Scanner class

---
+++ code_batch6/src/com/basic/ScannerEvenodd.java
@@ -0,0 +1,24 @@
+package com.basic;
+
+import java.util.Scanner;
+
+public class ScannerEvenodd {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner (System.in);
+		System.out.println("enter the number");
+		int num=sc.nextInt();
+		if(num%2==0) 
+		{ 
+			System.out.println("Number is even");
+		}
+		else 
+		{
+			System.out.println("number is even");
+		}
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+
