b5a4d9b0f473a27315fc6136e1f5fbbc0f224daf
Hemraj <Hemraj@DESKTOP-U0HKUMH>
2023-03-23 19:41:08 +0530

    commit

---
+++ demo/src/com/demo/Addition.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    int a = 45;
+    int b = 80;
+    int c = a+b;
+    System.out.println(c);
+
+}
+}
