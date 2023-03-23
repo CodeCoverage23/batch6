77b24b4972276daf7966f79e08b2a4b5c9871cdc
WIN10 <WIN10@DESKTOP-NTBHFME>
2023-03-23 18:15:55 +0530

    if else

---
+++ demo/src/com/demo/IfElse.java
@@ -0,0 +1,39 @@
+package com.demo;
+
+public class IfElse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*int a=21;
+		 if( a %2==0) {
+			 
+			 System.out.println("This is the even value.");
+			 
+			  }
+		 else {
+			 
+			 System.out.println("this is the Odd value");
+		 }*/
+			
+		int a=10;
+		int b=20;
+		int c=20;
+		
+		if(a<b && b==c) {
+			
+			int d=a+b+c;
+			System.out.println(d);
+		}
+		
+		else {
+			
+			System.out.println("condition falis");
+		}
+
+		
+
+
+	}
+
+}
