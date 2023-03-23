9ea75652d1cdb76304e4ed0772cbda92014fffdf
WIN10 <WIN10@DESKTOP-NTBHFME>
2023-03-23 18:17:49 +0530

    addition

---
+++ demo/src/com/demo/Addition.java
@@ -0,0 +1,18 @@
+package com.demo;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=10;
+		int b=12;
+		//int c=a+b;
+		int c=a-b;
+		
+		System.out.println(c++);
+		System.out.println(c);
+
+	}
+
+}
