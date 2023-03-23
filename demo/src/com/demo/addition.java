5015bfcea03a6178461690e4357f58c979d9d643
m <m@LAPTOP-8R9QEE6A>
2023-03-23 14:46:30 +0530

    Addition

---
+++ demo/src/com/demo/addition.java
@@ -0,0 +1,14 @@
+package com.demo;
+
+public class addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int a= 15;
+        int b= 25;
+        int c= a+b;
+        
+        System.out.println(c);
+	}
+
+}
