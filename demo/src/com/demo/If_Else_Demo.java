c6b33e746050ccf88bb491015a3119b229af1c4e
mansi <mansi@ASUS>
2023-03-23 23:51:18 +0530

    IfElse_Update

---
+++ demo/src/com/demo/If_Else_Demo.java
@@ -0,0 +1,16 @@
+package com.demo;
+
+public class If_Else_Demo {
+	
+	public static void main(String[] args) {
+		
+		int age = 16;
+		
+		if(age<18) {
+			System.out.println("Your age is below 18");
+		}else {
+			System.out.println("Congrats you are 18+");
+		}
+	}
+	
+}
---
+++ demo/src/com/demo/Odd_Even_If_Else.java
@@ -0,0 +1,16 @@
+package com.demo;
+
+public class Odd_Even_If_Else {
+
+	public static void main(String[] args) {
+		int a = 125;
+		
+		if(a%2==0) {
+			System.out.println(a + " is even number");
+		}
+		else {
+			System.out.println(a + " is odd number");
+		}
+		
+	}
+}
