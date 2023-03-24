3afad3e2def4dfe79daab91b9035392954d0a040
mansi <mansi@ASUS>
2023-03-24 15:42:31 +0530

    if else ladder upload

---
+++ demo/src/com/demo/If_Else_Ladder.java
@@ -0,0 +1,31 @@
+package com.demo;
+
+import java.util.Scanner;
+
+public class If_Else_Ladder {
+	
+	public static void main(String[] args) {
+		
+		Scanner scan =new Scanner (System.in);
+		System.out.println("Enter Your Name here");
+		String name =scan.next();
+//		int age = 5;
+		
+		System.out.println("Enter Your Age here");
+		int age = scan.nextInt();
+		
+		if (age<5) {
+			System.out.println("You are a kidd. ");
+		}else if(age>=5 && age<=15) {
+			System.out.println("According to your age You are in School");
+		}else if(age>=16 && age<=18){
+			System.out.println("According to your age you are in Junior College");
+		}else if(age>=19 && age<=22){
+			System.out.println("According to your age you are in graduation");
+		}else  {
+			System.out.println("Now You are ready for Job");
+		}
+		
+	}
+
+}
