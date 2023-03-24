727c8cb82d547d64cb14c2a4b435dc20a9013429
m <m@LAPTOP-8R9QEE6A>
2023-03-24 18:44:54 +0530

    If_Else using Scanner class

---
+++ demo/src/com/demo/basic_if_else.java
@@ -0,0 +1,25 @@
+package com.demo;
+import java.util.Scanner;
+
+public class basic_if_else {
+	
+       public static void main (String[]args)  {
+    	   
+    	Scanner sc=new Scanner (System.in);
+    	
+   		System.out.println("enter the number");
+   		
+   		int num=sc.nextInt();
+   		if(num%2==0) 
+   		{ 
+   			System.out.println("Number is even");
+   		}
+   		else 
+   		{
+   			System.out.println("number is odd");
+   		}
+   		}   
+    	   
+    	   
+
+}
