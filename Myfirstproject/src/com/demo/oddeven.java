f69fbfe0f7d2c770096cf7ed7fc6d5bdf24e68f5
Lenovo <Lenovo@lenovo>
2023-03-24 22:44:43 +0530

    Oddeven number

---
+++ Myfirstproject/src/com/demo/oddeven.java
@@ -0,0 +1,24 @@
+package com.demo;
+
+public class oddeven {
+
+	public static void main(String[] args) {
+		
+      int a=13;
+      
+      if(a==0) {
+    	  
+    	  System.out.println(a+ " is a whole number ");
+      }
+      else if(a%2==0) {
+    	  
+    	  System.out.println(a+ " is an odd number ");
+      }
+      else {
+    	  
+    	  System.out.println(a+ " is an even number ");
+      }
+      
+	}
+
+}
