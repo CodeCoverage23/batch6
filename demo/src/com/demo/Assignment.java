003cbde492f693b35b8543b9f625217d108e80bd
m <m@LAPTOP-8R9QEE6A>
2023-03-24 19:43:39 +0530

    Assignment And Relational

---
+++ demo/src/com/demo/Assignment.java
@@ -0,0 +1,24 @@
+package com.demo;
+
+public class Assignment {
+      public static void main (String []args) {
+    	 
+    			// TODO Auto-generated method stub
+
+    			int a=15;
+    			int b=25;
+    			
+    			System.out.println(a);
+ //   			a+=10;//a=a+10
+    			a+=b;//a=a+b;
+  //  			a-=5;//a=a-5;a=10-5;
+    			System.out.println(a);
+      
+      
+   
+      
+      
+     
+   
+      }
+}
---
+++ demo/src/com/demo/Relation.java
@@ -0,0 +1,20 @@
+package com.demo;
+
+public class Relation {
+          
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=20;
+		int b=12;
+		
+		System.out.println(a>b);
+		System.out.println(a<b);
+		System.out.println(a>=b);
+		System.out.println(a<=b); 
+		System.out.println(a==b);
+		System.out.println(a!=b);  
+		System.out.println(a);
+		System.out.println(b);
+}
+}
