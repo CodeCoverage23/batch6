f8c47625221c869abc7da19d4bb687cd2e8a8569
hp <hp@DESKTOP-7OTSBFL>
2023-03-25 16:02:39 +0530

    if else ladder

--- demo/src/com/demo/IfLadder.java
+++ demo/src/com/demo/IfLadder.java
@@ -6,28 +6,30 @@ public class IfLadder {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//  	int marks =10;
+//      int marks =10;
 		Scanner scanner= new Scanner(System.in);
 		System.out.println("enter a value :");
 		int marks=scanner.nextInt();
 		if(marks>=80 && marks<=90) {
 			System.out.println("grade A++");
 		}
-		else if(marks>=70 && marks<80)
-		{
+		else if(marks>=70 && marks<80) {
 			System.out.println("grade A");
-			
 		}
 		else if(marks>=60 && marks<70) {
-			System.out.println("geade B");
+			System.out.println("grade B");
 		}
 		else if(marks>=50 && marks<60) {
 			System.out.println("grade C");
 		}
 		else if(marks>=40 && marks<50) {
-			System.out.println("Failed");
+			System.out.println("failed");
 		}
-			
+		else
+		{
+			System.out.println("worst student");
+		}
+		
 	}
 
 }
