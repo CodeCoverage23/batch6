be6a8692a84497ebdf77e37ceafe419fc1f5d9dc
GALAXY <GALAXY@DESKTOP-OBKU59L>
2023-03-25 11:15:48 +0530

    IF ELSE LAADER

--- code_batch6/src/com/basic/Ifelseladderage.java
+++ code_batch6/src/com/basic/Ifelseladderage.java
@@ -6,48 +6,36 @@ public class Ifelseladderage {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        Scanner sc =  new Scanner (System.in);
-        
-        System.out.println("enter addhar no");
-        int no = sc.nextInt();
-        System.out.println("enter the name ");
-        String name = sc.next();       
-         System.out.println("enter the Age");
-         int age =sc.nextInt();
-         if (age>=50 && age <70 )
-         {
-        	 System.out.println("senior citizen");
-         }
-           else if(age>=50 && age<60) {
-        	 System.out.println("Younger Citizen");
-         }
-           else if(age>=40 && age<50) 
-           {
-        	   System.out.println("Young  Middle Age Adult ");
-           }
-           else if (age>=20 && age<30 ) {
-        	   System.out.println("Adult ager");
-        	   
-           }else if(age>=10 && age<20) {
-        	   System.out.println("teen ager");
-        	  
-           }
-         
-           else if (age>=5 && age <10) {
-        	   System.out.println("kid Ager");
-        	   }
-           else if (age>=0 && age<5) {
-        	   System.out.println("child ager");
-           }
-         
-           }
-         
-         
-        	   
-         
-         
-        
-               
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("enter addhar no");
+		int no = sc.nextInt();
+		System.out.println("enter the name ");
+		String name = sc.next();
+		System.out.println("enter the Age");
+		int age = sc.nextInt();
+		if (age >= 50 && age < 70) {
+			System.out.println("senior citizen");
+		} else if (age >= 50 && age < 60) {
+			System.out.println("Younger Citizen");
+		} else if (age >= 40 && age < 50) {
+			System.out.println("Young  Middle Age Adult ");
+		} else if (age >= 20 && age < 30) {
+			System.out.println("Adult ager");
+
+		} else if (age >= 10 && age < 20) {
+			System.out.println("teen ager");
+
+		}
+
+		else if (age >= 5 && age < 10) {
+			System.out.println("kid Ager");
+		} else if (age >= 0 && age < 5) {
+			System.out.println("child ager");
+		} 
+		else
+			System.out.println("invalid age");
+
+	}
 
 }
-
