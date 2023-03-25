69296b85be2e63fbdb38e8744d97630bf3695f31
Hemraj <Hemraj@DESKTOP-U0HKUMH>
2023-03-25 16:21:29 +0530

    TodayCommit

---
+++ demo/src/com/demo/BankingSwitchProgram.java
@@ -0,0 +1,43 @@
+package com.demo;
+
+public class BankingSwitchProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   String sr = "HDFC bank";
+    switch (sr) {
+    case "ICICI":
+    	System.out.println("ICICI bank");
+    	break;
+    case "punjab bank":
+    	System.out.println("punjab bank");
+    	break;
+    case"bank of maha":
+    	System.out.println("bank of maha.");
+    	break;
+    case"Axis bank":
+    	System.out.println("Axis bank");
+    	break;
+    case"Union bank":
+    	System.out.println("Union bank");
+    	break;
+    case "HDFC bank":
+    	System.out.println("HDFC bank");
+    	break;
+    case"Kotak bank":
+    	System.out.println("Kotak bank");
+    	break;
+    case"Central bank":
+    	System.out.println("Central bank");
+    	break;
+    case"Sbi bank":
+    	System.out.println("Sbi bank");
+        break;
+        default:
+    		System.out.println("emity");
+    
+    	
+    }
+	}
+
+}
