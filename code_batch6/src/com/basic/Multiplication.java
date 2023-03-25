6d26347e0de33046b2a11c05496284d0394f8024
asus <asus@LAPTOP-LN2QCEDH>
2023-03-25 15:27:30 +0530

    Multiplication

---
+++ code_batch6/src/com/basic/Multiplication.java
@@ -0,0 +1,14 @@
+package com.basic;
+
+public class Multiplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=12;
+int b=7;
+int c=(a*b);
+System.out.println(c);
+
+	}
+
+}
