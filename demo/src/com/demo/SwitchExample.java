fc3347c20a20491d7fb09bb39895ffde124a2c95
prade <prade@DESKTOP-NS5DLR9>
2023-03-25 09:42:54 +0530

    for loop and switch case

---
+++ demo/src/com/demo/SwitchExample.java
@@ -0,0 +1,43 @@
+package com.demo;
+
+public class SwitchExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int number=6;
+		String color ="";
+		switch(number) {
+		case  1: 
+			color="red";
+			break;
+		case 2:
+			color="yellow";
+			break;
+		case 3:
+			color="blue";
+			break;
+		case 4:
+			color="green";
+		break;
+			default:
+				color="not available";
+		}
+		System.out.println(color);
+		
+		
+		
+		
+		
+		
+		
+		}
+		
+		
+		
+		
+		
+		
+		
+	}
+
+
