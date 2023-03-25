572d0fe98a870f6574835b1f0184b89254e8a7fc
User <User@DESKTOP-OQ2KTH4>
2023-03-25 19:52:12 +0530

    datatypes

---
+++ code_batch6/src/com/Demo/data_types.java
@@ -0,0 +1,25 @@
+package com.Demo;
+
+public class data_types {
+  public static void main(String[] args) {
+		// numeric (integer)
+		int a=10;  //size of int is 
+		byte b=20; //-127 to 128 range
+		short s=30; //
+		long l=40; //
+		//floating 
+		float f=20.13f; //it contain the pointing value
+		double d=49.78; //it contain the double value after .
+		//non numeric;
+		char c  ; //it contain alphabet
+		boolean b1 = false; //true/false 
+        System.out.println(a);
+		System.out.println(b);
+		System.out.println(s);
+		System.out.println(l);
+		System.out.println(f);
+		System.out.println(d);
+		System.out.println("tanvi");
+		System.out.println(b1);
+	}
+}
