13c8583f2797f0059c230e80785e8ee5835074e2
asus <asus@LAPTOP-LN2QCEDH>
2023-03-25 22:11:35 +0530

    Division

---
+++ code_batch6/src/com/basic/Division.java
@@ -0,0 +1,14 @@
+package com.basic;
+
+public class Division {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=24;
+int b=6;
+int c=a/b;
+System.out.println(c);
+
+	}
+
+}
