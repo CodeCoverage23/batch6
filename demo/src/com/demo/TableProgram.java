83233a0235ade58d01803fc89c53b068af07505f
User <User@DESKTOP-OQ2KTH4>
2023-03-26 20:13:46 +0530

    table print 2nd to 10th program

---
+++ demo/src/com/demo/TableProgram.java
@@ -0,0 +1,17 @@
+package com.demo;
+
+public class TableProgram {
+public static void main(String[] args) {
+	
+	int num = 1;
+    for(int i=1;i<=10;i++)
+    {
+        for(int j=2;j<=10;j++)
+        {
+            num = i*j;
+            System.out.print(num+" ");
+        }
+        System.out.println();
+    }
+}
+}
