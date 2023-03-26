bfd28018a520d5ca1764a15252d4dfa7a8aeeace
SurajDotNetDeveloper <surajcool9623@gmail.com>
2023-03-26 18:24:22 +0530

    update

--- code_batch6/src/com/basic/VariableDemo.java
+++ code_batch6/src/com/basic/VariableDemo.java
@@ -2,19 +2,22 @@ package com.basic;
 
 public class VariableDemo {
 
-	static int data = 10; // static variable
-
-//	public void method() {
-//		int d = 10; // local variable
-//	}
-//
-//	public void method1() {
-//		System.out.println(data);
-//	}
-
+	static int a=10;
+	static int b=20;		
+	static float f=34.398880987799f;
+	static byte y=3;
+	double d=2345678;
 	public static void main(String[] args) {
-		int b = 11;
-		int c = data + b;
-		System.out.println(c);
+		// TODO Auto-generated method stub
+		
+		long l= 23456788;
+		short s=678;
+		boolean e= false ;
+		 char c= 'P';
+		 System.out.println(a);
+		 System.out.println(b);
+		 System.out.println(f);
+		 System.out.println(y);
+		 System.out.println(e);
 	}
 }
