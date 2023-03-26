189ec63f4966f784b9ac13a3fb4b22c2fe0d000d
Lenovo <Lenovo@lenovo>
2023-03-26 20:11:06 +0530

    new updated printtable

--- code_batch6/src/com/basic/printTable.java
+++ code_batch6/src/com/basic/printTable.java
@@ -4,13 +4,17 @@ public class printTable {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int num=1;
+		
 		for(int i=1; i<=10;i++) {
 			
-			for(int j=1;j<=10;j++) 
+			for(int j=2;j<=10;j++) 
 			{
-	    	System.out.println((i * (j)));
+	    	num=i*j;
+	    	System.out.print(num+" ");
 	    	
 	    	}
+			
 	        System.out.println();
 	}
 
