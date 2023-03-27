d10c16e487b9127007642d631a1c7932f8033787
hp <hp@DESKTOP-7OTSBFL>
2023-03-27 21:23:27 +0530

    table

---
+++ demo/src/com/demo/tableprogram.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import java.util.Scanner;
+
+public class tableprogram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     
+    
+	}
+
+}
