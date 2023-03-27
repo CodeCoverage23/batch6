ba8970224889bb5776599eccd5c571e8a9c6b687
lenovo <lenovo@DESKTOP-PF8O34S>
2023-03-27 19:54:34 +0530

    for loop multiplication table using scanner class

---
+++ code_batch6/src/com/basic/ForLoopmultiplicationtable.java
@@ -0,0 +1,22 @@
+package com.basic;
+
+import java.util.Scanner;
+
+public class ForLoopmultiplicationtable {
+
+	public static void main(String[] args) {
+		Scanner ak =new Scanner(System.in);
+		System.out.println("Enter the number:");
+		//int n=ak.nextInt();
+		int n=ak.nextInt();
+		for(int i=1;i<=10;i++) 
+		{
+		
+			System.out.println(n+" * "+i+" = "+n*i);
+		}
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+
