9f46169eaced4df6ba0fb279ca8a5ef59e1c0f33
mansi <mansi@ASUS>
2023-03-27 21:32:55 +0530

    Name series for loop update

---
+++ demo/src/com/demo/For_Loop_Name_series_HW.java
@@ -0,0 +1,21 @@
+package com.demo;
+
+public class For_Loop_Name_series_HW {
+	public static void main(String[] args) {
+		
+		for(int i=1;i<=100;i++) {
+			if(i%(5*7)==0) {
+				System.out.println("Mansi Sahu");
+			}else if(i%7==0) {
+				System.out.println("Sahu");
+			}else if(i%5==0){
+				System.out.println("Mansi ");
+			}else {
+				System.out.println(i);
+			}
+			
+			
+		}
+		
+	}
+}
