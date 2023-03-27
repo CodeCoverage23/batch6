679383a866d40ded86335ea16353ce9bee77f792
lenovo <lenovo@DESKTOP-PF8O34S>
2023-03-27 19:49:34 +0530

    switch case using month and season

---
+++ code_batch6/src/com/basic/Switchdemo.java
@@ -0,0 +1,40 @@
+package com.basic;
+
+import java.util.Scanner;
+
+public class Switchdemo {
+
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+
+//			int age = 20;
+			System.out.println("Enter your age");
+			Scanner scanner = new Scanner(System.in);
+			int nextInt = scanner.nextInt();
+			String msg = "";
+			switch (nextInt) {
+			case 16:
+				msg = "you are under 18";
+				break;
+			case 18:
+				msg = "you are eligible for vote";
+				break;
+			case 65:
+				msg = " you are senior citzen";
+				break;
+			default:
+				msg = "invalid age";
+				break;
+
+			}
+			System.out.println(msg);
+		}
+
+	
+
+		// TODO Auto-generated method stub
+
+	}
+
+
