8991e8853ca3dabd67dbcb499868c0b2a433e1da
User <User@DESKTOP-OQ2KTH4>
2023-03-27 19:28:10 +0530

    for loop

---
+++ code_batch6/src/com/Demo/for_loop.java
@@ -0,0 +1,20 @@
+package com.Demo;
+
+public class for_loop {
+
+	public static void main(String[] args) {
+		
+		int num = 2;
+		
+		for(int a=1;a<=10;a++)
+		     {
+		     for(int b=1;b<11;b++) {
+		    	 num=a*b;
+			System.out.print(num+" ");
+	    }
+		System.out.println();
+	}
+	}
+}
+
+
