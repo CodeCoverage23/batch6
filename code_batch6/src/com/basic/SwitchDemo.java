c651d72690e4ba26f37bce9e4d586be65f12aad6
GALAXY <GALAXY@DESKTOP-OBKU59L>
2023-03-28 07:56:48 +0530

    Switch

---
+++ code_batch6/src/com/basic/SwitchDemo.java
@@ -0,0 +1,39 @@
+ package com.basic;
+
+public class SwitchDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+				
+				int age=21;
+				String msg ="";
+				switch(age) {
+				case 16:msg="you are under18";
+				break;
+				case 18:msg="you are eligible for vote";
+				 break;
+				case 65:msg="you are senior citizen";
+				 break;
+				 default:msg= "invalid voter";
+				   break; 
+					
+			}
+			 System.out.println(msg);
+			 
+			
+
+
+
+
+
+
+
+
+
+			}
+			
+
+	}
+
+
