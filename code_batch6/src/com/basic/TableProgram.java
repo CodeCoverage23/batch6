a7c04b9c777a76474d45fc5a7551dbd7efa3408b
user <user@LAPTOP-9K2SO253>
2023-03-28 15:37:43 +0530

    Table form

---
+++ code_batch6/src/com/basic/TableProgram.java
@@ -0,0 +1,21 @@
+package com.basic;
+
+public class TableProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int num = 0;
+        for(int i=1;i<=1;i++)
+        {
+        	for(int j=0;j<=100;j++)
+        	{
+        		num = i+j;
+        		System.out.println(num+" ");
+        		
+        }
+		System.out.println();
+		
+	}
+
+	}
+}
