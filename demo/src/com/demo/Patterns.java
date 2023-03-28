6003985f3fe5479218942a61cade43e9060ba114
Manthan Aage <Manthan Aage@LAPTOP-09BEGS6L>
2023-03-28 17:51:37 +0530

    'Star patterns' code

---
+++ demo/src/com/demo/Patterns.java
@@ -0,0 +1,21 @@
+package com.demo;
+import java.util.*;
+
+public class Patterns {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+        System.out.println("enter the number");
+          int rows=sc.nextInt();
+          for(int i=1;i<=rows;i++) {
+        	  for( int j=1;j<=5;j++) {
+        		  
+        		  System.out.print("* ");
+        		  
+        	  }System.out.println();
+              
+          }
+         
+	}
+
+}
