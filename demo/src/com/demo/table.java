35857b202fa1014df1985798292280f21e26d26e
hp <hp@DESKTOP-7OTSBFL>
2023-03-28 09:35:43 +0530

    table

---
+++ demo/src/com/demo/table.java
@@ -0,0 +1,20 @@
+package com.demo;
+
+public class table {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int num = 1 ;
+		for(int i=1;i<=10;i++)
+        {
+			for(int j=2;j<=10;j++)
+			{
+				num = i*j;
+				System.out.println(num+" ");			
+				}
+		
+		System.out.println(" ");
+        }
+}
+}
