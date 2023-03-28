effae7f1110de3e94f55edd0790349ebace1f6f3
dell <dell@DESKTOP-I2B4679>
2023-03-28 19:02:02 +0530

    Starpattern

---
+++ demo/src/com/demo/StarPatternForLoop.java
@@ -0,0 +1,20 @@
+package com.demo;
+
+public class StarPatternForLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for (int a=0; a<5; a++) {
+			for (int b=5-a; b>1; b--) {
+				System.out.print(" ");
+			}
+			for (int c=0; c<=a; c++) {
+				System.out.print(" * ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
