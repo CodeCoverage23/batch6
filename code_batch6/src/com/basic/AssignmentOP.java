666c545674aec3926daa938cfe0066e1a593cca2
SurajDotNetDeveloper <surajcool9623@gmail.com>
2023-03-28 15:35:20 +0530

    assignment

---
+++ code_batch6/src/com/basic/AssignmentOP.java
@@ -0,0 +1,30 @@
+package com.basic;
+
+public class AssignmentOP {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a= 101;
+int b = 201;
+  System.out.println(a);
+  a+=10;
+  b+=20;
+  System.out.println(a);
+  System.out.println(b);
+	a-=5;
+	b-=32;
+	System.out.println(a);
+	System.out.println(b);
+	System.out.println("rest of part:");
+	System.out.println("privious value :"+a);
+	System.out.println(a+"next value:");
+	System.out.println(a+b+"privious value:");
+	System.out.println("my value:"+a+b);
+	System.out.println(a+b);
+	System.out.println(a-b);
+	System.out.println(a);
+	System.out.println(b);
+	
+	}
+
+}
