45af972132a89f98e5c428a60f67c936ae2b58b6
Manthan Aage <Manthan Aage@LAPTOP-09BEGS6L>
2023-03-28 18:27:33 +0530

    'pyramid pattern' code

---
+++ demo/src/com/demo/pyramidpattern.java
@@ -0,0 +1,23 @@
+package com.demo;
+public class pyramidpattern{
+	public static void main(String[] args) {
+		char x=65;
+		for(int i=0;i<5;i++) {
+			for(int k=5-i;k>1;k--) {
+				
+				System.out.print(" ");
+				
+				
+			}for(int j=0;j<=i;j++) {
+				System.out.print(x );
+				System.out.print(" ");
+				x++;
+			}
+			System.out.println();
+			
+		}
+		
+		
+	}
+	
+}
