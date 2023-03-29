d027c044b67927b40e076a5b6555c23317060ae0
rukshadyn <ASUS@LAPTOP-BAUI3IHS>
2023-03-29 17:34:04 +0530

    starpattern and while loop

---
+++ code_batch6/src/com/basic/WhileLoop.java
@@ -0,0 +1,18 @@
+package com.basic;
+
+public class WhileLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=1;
+		
+		while (a<=6)
+		{
+			int b=a+1;
+			System.out.println(b);
+			a++;
+		}
+	}
+
+}
