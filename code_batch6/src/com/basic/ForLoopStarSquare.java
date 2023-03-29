2f97f719e1c397e0588c3d362a8ddfe229635a5a
lenovo <lenovo@DESKTOP-PF8O34S>
2023-03-29 13:05:41 +0530

    For loop Square Star program

---
+++ code_batch6/src/com/basic/ForLoopStarSquare.java
@@ -0,0 +1,22 @@
+package com.basic;
+
+
+
+public class ForLoopStarSquare {
+
+	public static void main(String[] args) {
+		int i, j, a;
+	      a = 12;
+	      System.out.println("The length of a side is defined as " +a);
+	      System.out.println("The square pattern : ");
+	      for(i = 1; i <= a; i++){
+	         for(j = 1; j <= a; j++){
+	            System.out.print("* ");
+	         }
+	         System.out.print("\n");
+	      }
+	   }
+	}
+
+	
+
