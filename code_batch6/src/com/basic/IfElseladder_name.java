f5d4fd59a35f84ab316c144e3987665f27a2374b
lenovo <lenovo@DESKTOP-PF8O34S>
2023-03-29 12:29:39 +0530

    First name Last name program

---
+++ code_batch6/src/com/basic/IfElseladder_name.java
@@ -0,0 +1,27 @@
+package com.basic;
+
+public class IfElseladder_name {
+
+	public static void main(String[] args) {
+	for(int i=1; i<=100;i++) {
+		
+		if(i%(5*7)==0){
+		System.out.println(i+ "Avinash khadse" );
+		//continue;
+		}
+	 else if (i%5==0) {
+		 System.out.println(i+ "Avinash" );
+	 }
+		 else if (i%7==0) {
+			 System.out.println( i + "Khadse" );
+		 }
+	 
+System.out.println(i);
+		 
+		}
+			
+
+	}
+
+	}
+ 
