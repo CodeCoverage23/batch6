5e32cbdbf8c81777e241cb42d85a38662bc5540f
lenovo <lenovo@DESKTOP-PF8O34S>
2023-03-29 13:07:54 +0530

    Square star paten

--- code_batch6/src/com/basic/ForLoopStarSquare.java
+++ code_batch6/src/com/basic/ForLoopStarSquare.java
@@ -7,7 +7,7 @@ public class ForLoopStarSquare {
 	public static void main(String[] args) {
 		int i, j, a;
 	      a = 12;
-	      System.out.println("The length of a side is defined as " +a);
+	      System.out.println("The Square length  " +a);
 	      System.out.println("The square pattern : ");
 	      for(i = 1; i <= a; i++){
 	         for(j = 1; j <= a; j++){
