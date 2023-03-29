789bbbada88bf320c6629e56574e43c52c942d41
Hemraj <Hemraj@DESKTOP-U0HKUMH>
2023-03-29 11:56:09 +0530

    Print

---
+++ demo/src/com/demo/PrintNumber.java
@@ -0,0 +1,41 @@
+package com.demo;
+
+public class PrintNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  for(int i = 1 ;i <=100; i++) {
+
+	  
+	  String st = "Gargi";
+	  
+	  
+	  if(i%5==0)  {
+
+	 System.out.print(" Gargi");
+	  
+  }
+	  String s = "Bardeo";
+	  if(i%7==0) {
+		  System.out.print(" Bardeo");
+	  }
+	  String str = "Gargi Bardeo";
+	  if(i%5*7==0) {
+		  System.out.print(i+" Gargi Bardeo");
+	  
+	  }	  
+		  
+	  
+	  System.out.println(i);
+ 
+ 
+		 
+	 
+}
+}
+}
+
+
+
+
+
