98dbb0e520b0e28555327a994aadfab8feaec36b
Manthan Aage <Manthan Aage@LAPTOP-09BEGS6L>
2023-03-29 17:26:14 +0530

    'Factorial' program

---
+++ demo/src/com/demo/Factorial.java
@@ -0,0 +1,17 @@
+package com.demo;
+ public class Factorial{
+	 public static void main(String[] args) {
+		 
+		 int i=1, factorial=1;
+		 
+		 for(i=1;i<=10;i++) {
+			 
+			 factorial *= i;
+			 
+			 System.out.println(factorial);
+		 }
+		 
+		 
+		 System.out.println();
+	 }
+ }
