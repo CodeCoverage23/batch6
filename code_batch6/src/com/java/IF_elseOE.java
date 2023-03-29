184e7ebebb0a2d0c533a2fd2f9bd108cf7dc5b8e
SurajDotNetDeveloper <surajcool9623@gmail.com>
2023-03-29 16:53:35 +0530

    if_else with odd-even example

---
+++ code_batch6/src/com/java/IF_elseOE.java
@@ -0,0 +1,25 @@
+package com.java;
+
+import java.util.Scanner;
+
+public class IF_elseOE {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ 
+Scanner gp = new Scanner (System.in);
+	      System.out.println("enter the value");
+	      int num=gp.nextInt();
+	
+	if (num % 2 == 0)
+	{
+	
+	System.out.println("number is even");
+	}
+	else
+	{  
+		System.out.println("number is odd");
+	}
+	}
+
+}
---
+++ code_batch6/src/com/java/If_else.java
@@ -0,0 +1,22 @@
+package com.java;
+
+public class If_else {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+             int p=10;
+             int q= 20;
+             int r= 30;
+             int s= 30;
+             
+            if (p<q||r==s) {;
+           int t = p+q;
+           System.out.println(t);
+	}
+          else 
+               {
+        	   System.out.println(" condition is failed");
+   			
+        	   
+               }
+}}
