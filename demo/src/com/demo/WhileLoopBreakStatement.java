e82caa70b7211555e400308ad3ac612408157be9
dell <dell@DESKTOP-I2B4679>
2023-03-29 19:24:53 +0530

    WhileLoop

---
+++ demo/src/com/demo/WhileLoopBreakStatement.java
@@ -0,0 +1,19 @@
+package com.demo;
+
+public class WhileLoopBreakStatement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=5;
+		while (a<=15) {
+			if (a==10) {
+				break;
+			}
+			System.out.println(a);
+			a++;
+		}
+
+	}
+
+}
