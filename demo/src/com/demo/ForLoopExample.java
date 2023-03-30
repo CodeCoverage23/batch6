4413fc5ade90a68defd193d32e2ec4c4444ac7f1
m <m@LAPTOP-8R9QEE6A>
2023-03-30 17:17:07 +0530

    2 to 10 table using ForLoop

---
+++ demo/src/com/demo/ForLoopExample.java
@@ -0,0 +1,26 @@
+package com.demo;
+
+public class ForLoopExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+           
+		
+		  int num;
+		  for(int a=1;a<=10;a++) {
+			for(int b=2;b<=10;b++)  {
+				
+				num=a*b;
+			
+					System.out.print(num+" ");
+				}
+				
+			System.out.println();
+				
+
+			}
+		}
+			
+	}
+
+
