0a5ab32a9dea4f3acbfdfbb5b25b268a900af183
User <User@DESKTOP-OQ2KTH4>
2023-03-30 19:37:28 +0530

    find Factorial numbers program

---
+++ demo/src/com/demo/FactorialProgram.java
@@ -0,0 +1,20 @@
+package com.demo;
+
+import java.util.Scanner;
+
+public class FactorialProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int num=sc.nextInt();
+     int fact=1;
+     for (int i=1;i<=num;i++) {
+    	 fact=fact*i;
+    	 
+    	 
+     }
+     System.out.println("factorial is: "+fact);
+	}
+
+}
