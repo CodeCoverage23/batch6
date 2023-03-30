1bd7df30fed8f73b98aa051a800b2de0f2a09079
admin <admin@DESKTOP-JV9L41V>
2023-03-30 17:52:46 +0530

    2program

---
+++ demo/src/com/demo/StarPattern.java
@@ -0,0 +1,40 @@
+package com.demo;
+
+public class StarPattern {
+
+	public static void main(String[] args) {
+		
+       int a, b, c;
+       for(a=1;a<=5;a++)
+       {
+    	   for(b=a;b<5;b++)
+    	   {
+    		   System.out.print(" ");
+    	   }
+           for(c=1;c<(a*2);c++)
+           {
+        	   System.out.print("*");
+           }
+           System.out.println();
+       }
+       for(a=4;a>=1;a--);
+       {
+    	   for(b=5;b>a;b--)
+    	   {
+    		   System.out.print(" ");
+    	   }
+		   for(c=1;c<(a*2);c++)
+		   {  
+			   System.out.print("*");
+           };
+		   System.out.println();
+       }
+    }
+}
+		
+		
+		
+		
+	
+
+
---
+++ demo/src/com/demo/StarPattern2.java
@@ -0,0 +1,20 @@
+package com.demo;
+
+public class StarPattern2 {
+
+	public static void main(String[] args) {
+		
+        int i,j;
+        for (i=1; i<=8; i++)
+        {
+        	for (j=1; j<i; j++)
+        	{
+        		System.out.print("*");
+        	}
+            System.out.println();
+        }
+    }
+}
+
+
+
