f47f5899f666bf334bd35bd116e12a1a21664d8d
User <User@DESKTOP-OQ2KTH4>
2023-03-31 19:37:42 +0530

    using constructor student info program

---
+++ demo/src/com/demo/ParameterisedConstructor.java
@@ -0,0 +1,23 @@
+package com.demo;
+
+public class ParameterisedConstructor {
+	int id;
+	String name;
+	String branch;
+	String collageName;
+	ParameterisedConstructor(int id,String name,String branch,String collageName){
+		this.id=id;
+		this.name=name;
+		this.branch=branch;
+		this.collageName=collageName;
+	}
+	public static void main(String[] args) {
+		ParameterisedConstructor p= new ParameterisedConstructor(01,"Arpita","cse","Kgiet");
+	   System.out.println("student id is: "+(p.id));
+	   System.out.println("student name is : "+p.name);
+	   System.out.println("student branch is : "+p.branch);
+	   System.out.println("student collage name is : "+p.collageName);
+	}
+	
+
+}
