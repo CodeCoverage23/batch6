75f9315c3ab97c6322a2ad8cfda97ff17a9b3e02
lenovo <lenovo@DESKTOP-PF8O34S>
2023-03-31 15:06:20 +0530

    calculator

---
+++ code_batch6/src/com/basic/Calculator.java
@@ -0,0 +1,28 @@
+package com.basic;
+
+import java.util.Scanner;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		Scanner ak=new Scanner(System.in);
+		System.out.println("Enter First Number : ");
+		int a = ak.nextInt();
+		System.out.println("Enter Second Number : ");
+		int b=ak.nextInt();
+		Calculator(a,b);
+	}
+
+	public static void Calculator(int a, int b) {
+		int sum =a+b;
+		int sub=a-b;
+		int mul= a*b;
+		int div= a/b;
+		
+		System.out.println("Addition is "+ sum);
+		System.out.println("Subtrtaction is "+ sub);
+		System.out.println("Multiplication is : "+mul);
+		System.out.println("Division is : "+div);
+		
+	}
+}
