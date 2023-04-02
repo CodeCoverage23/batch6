4885cd76bc4b0cde0d92b48b8f3192198209cdc0
m <m@LAPTOP-8R9QEE6A>
2023-04-02 14:59:16 +0530

    Prime Numbers 1 to 100

---
+++ demo/src/com/demo/PrimeNumber.java
@@ -0,0 +1,25 @@
+package com.demo;
+
+public class PrimeNumber {
+
+	public static void main(String[] args) {
+		
+		int m=0,n=0,i=1,j=1;  
+		while(n<25) {  
+		j=1;  
+		m=0;  
+		while(j<=i) {  
+		if(i%j==0)  
+		m++;  
+		j++;   
+		}  
+		if(m==2){ 
+		System.out.printf("%d ",i);  
+		n++;  
+		}  
+		i++;  
+		}    
+	   }  
+	}
+
+
