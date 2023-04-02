0928a8b235a688436bc3daae04d7e7f9d65fa717
m <m@LAPTOP-8R9QEE6A>
2023-04-02 14:58:11 +0530

    Method Calculator

---
+++ demo/src/com/demo/MethodCalculator.java
@@ -0,0 +1,28 @@
+package com.demo;
+
+import java.util.Scanner;
+
+public class MethodCalculator {
+        
+   	public static void main(String[] args) {
+      	Scanner sc=new Scanner(System.in);
+     	System.out.println("Enter First Number : ");
+        int a = sc.nextInt();
+        System.out.println("Enter Second Number : ");
+        int b=sc.nextInt();
+        Calculator(a,b);
+         }
+        public static void Calculator(int a, int b) {
+        	int sum =a+b;
+        	int sub=a-b;
+        	int mul= a*b;
+        	int div= a/b;
+
+        System.out.println("Addition is : "+ sum);
+        System.out.println("Subtrtaction is : "+ sub);
+       	System.out.println("Multiplication is : "+ mul);
+       	System.out.println("Division is : "+ div);
+
+        	}
+        }
+
