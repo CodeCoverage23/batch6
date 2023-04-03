f9bdfa996f0a99e1e23a8adf679b75d599f8ca0a
lenovo <lenovo@DESKTOP-PF8O34S>
2023-04-03 21:31:42 +0530

    Calculator program

---
+++ code_batch6/src/com/basic/MethodCalculator.java
@@ -0,0 +1,42 @@
+package com.basic;
+
+import java.util.Scanner;
+
+public class MethodCalculator {
+	
+	
+	public MethodCalculator() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int  MethodCalculator(int a,int b) {
+		int c=a+b;
+		//int c1= a-b;
+		//int  c2= a*b;
+		//int c3= a/b;
+		
+	return c; 
+	}
+//		System.out.println("the sum of two number is "+(a+b));
+//		System.out.println("the sub of two number is "+(a-b));
+//		System.out.println("the mul of two number is "+(a*b));
+//		System.out.println("the div two number "+(a/b));
+	//}
+
+	public static void main(String[] args) {
+		MethodCalculator ak=new MethodCalculator();
+		 int result =ak.MethodCalculator (5,5);
+		 System.out.println("Addition Is"+ result);
+		
+		
+		 
+		 
+		 }
+}
+	
+
+	
+		// TODO Auto-generated methub
+
+
+
