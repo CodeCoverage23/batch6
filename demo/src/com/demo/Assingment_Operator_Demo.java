b40bb96900d7a158e378755ea18438625f9b04fc
mansi <mansi@ASUS>
2023-04-03 19:59:18 +0530

    update

---
+++ demo/src/com/demo/Assingment_Operator_Demo.java
@@ -0,0 +1,15 @@
+package com.demo;
+
+public class Assingment_Operator_Demo {
+
+	public static void main(String[] args) {
+		int a = 6;
+
+		 a+=6; //12 a=a+6
+		 a-=2; //10
+		 a*=5; //50
+		 a/=25; //2
+		
+		System.out.println(a);
+	}
+}
---
+++ demo/src/com/demo/For_Loop_Demo.java
@@ -0,0 +1,15 @@
+package com.demo;
+
+public class For_Loop_Demo {
+	static int b;
+	static int i;
+	public static void main(String[] args) {
+		
+		for (int i = 1; i <= 10; i++) {
+			b=2*i;
+			System.out.println(b);
+			
+		}
+
+	}
+}
