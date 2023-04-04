8ed3529aa6b8199e8cc88b97ee13376ac06ff7df
admin <admin@DESKTOP-JV9L41V>
2023-04-04 17:29:44 +0530

    1 program

---
+++ demo/src/com/demo/DemoArithmetical.java
@@ -0,0 +1,24 @@
+package com.demo;
+
+public class DemoArithmetical {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	   int a = 25;
+	   int b = 10;
+	   
+	    System.out.println(a-b);//15
+	    System.out.println(a*b);//250
+	    System.out.println(a+b);//35
+	    System.out.println(a%b);//0
+	    }
+    }
+
+
+
+
+
+
+
+
