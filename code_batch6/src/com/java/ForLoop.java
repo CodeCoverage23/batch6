54f7eb77b18f2783b486549fe090730a4cbd0557
SurajDotNetDeveloper <surajcool9623@gmail.com>
2023-04-04 18:38:18 +0530

    for loop with table

---
+++ code_batch6/src/com/java/ForLoop.java
@@ -0,0 +1,16 @@
+package com.java;
+
+public class ForLoop {
+	// static int a = 1;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int b;
+		for (int a = 1; a <= 20; a++) {
+			b = a * 2;
+			System.out.println(b);
+		}
+
+	}
+
+}
---
+++ code_batch6/src/com/java/ForTable.java
@@ -0,0 +1,28 @@
+package com.java;
+
+public class ForTable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int num ;
+for(int i=1;i<=10;i++) {
+	for( int j=2;j<=30;j++) {
+	num=j*i;
+	System.out.print(num+" ");}
+	{
+	System.out.println();
+	}
+	
+	
+	
+}
+
+
+
+
+
+
+
+	}
+
+}
