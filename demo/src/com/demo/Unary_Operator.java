4c432b90108de5dbf05a44a0b6facc80d1dcbb20
mansi <mansi@ASUS>
2023-04-05 15:47:03 +0530

    updated

---
+++ demo/src/com/demo/Unary_Operator.java
@@ -0,0 +1,59 @@
+package com.demo;
+
+public class Unary_Operator {
+	
+	public static void main(String[] args) {
+
+		int a = 10;
+		int b = 15;
+		boolean c = true;
+		boolean d = false;
+		
+//		System.out.println(a++);// a+1 =10
+//		System.out.println(a);
+		
+								//post fix unary operator
+		
+//		System.out.println(a++ + b++); // a+1 + b+1 = 10+15=25
+//		System.out.println(a); //after above step the value will be changed after increment by 1
+//		System.out.println(b);
+		
+//		System.out.println(a--); //a-1 =10
+//		System.out.println(a); // 9 after above step the value will be changed after decrement by 1
+		
+//		System.out.println(a-- - b--); //(a-1) - (b-1)= (10-1)-(15-1)= -5
+//		System.out.println(a); //9
+//		System.out.println(b); //14
+		
+								//prefix unary operator
+//		System.out.println(++a + ++b); //1+a + 1+b = 11+16+27
+		
+//		System.out.println(--a - --b); //  10-1 - 15-1 = 9 - 14 = -5
+//		System.out.println(a); //9
+//		System.out.println(b); //14
+		 
+						//~  & ! it means opposite
+		
+//		   (~a) = -(a-1)  // in short we can say the original value added by 1 and give it - sign
+//				= -a+1
+//      		= - (a) -1
+//				=- 10 - 1
+//				= -11
+//				System.out.println(~a); //-11
+		
+//			(~b)= - (b-1)
+//				= - b + 1
+//				= - (b)-1
+//				= - 15 - 1
+//				= -16
+//		System.out.println(~b); //-16
+
+
+//		System.out.println(!c); //false
+//		System.out.println(!d); // true
+		
+		
+		
+	
+	}
+}
