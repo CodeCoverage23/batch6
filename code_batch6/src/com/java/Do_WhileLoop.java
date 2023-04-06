e6c4402ba454f4586df474814e7cfb9ad87844ec
GaneshJavaDeveloper <ganesgkp1075@gmail.com>
2023-04-06 17:40:01 +0530

    Do while loop

---
+++ code_batch6/src/com/java/Do_WhileLoop.java
@@ -0,0 +1,53 @@
+package com.java;
+
+public class Do_WhileLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stu
+
+                      		//infinite Ex:
+//		int z=20;
+//		while(z>10) {
+//			z+=20;
+//			System.out.println(z);
+//		}
+//		
+
+	                        	// simple Ex :
+//		int z=1;
+//		while (z<=5) {
+//			int b=z+1;
+//			System.out.println(b);	
+//			z++;
+//		}
+                       //		     continue
+//		int a = 1;
+//		while (a <= 10) {
+//			if (a == 4) {
+//				a++;
+//				continue;
+//			}
+//			System.out.println(a);
+//			a++;
+//		}
+//	}
+//}
+	                              	// Do_while
+//		int a =1;
+//		do {
+//			System.out.println(a);
+//			a++;}
+//			
+//		 while (a <= 20);}
+//}
+                               //			break 
+		int a = 1;
+		while (a <= 25) {
+			if (a == 15) {
+				break;
+			}
+			System.out.println(a);
+			a++;
+		}
+	}
+}
