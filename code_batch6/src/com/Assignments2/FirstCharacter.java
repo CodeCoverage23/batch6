5ef4486e5fbf83efa4950f4989abf510f2c4c91e
lenovo <lenovo@DESKTOP-PF8O34S>
2023-04-09 18:18:30 +0530

    First character print

---
+++ code_batch6/src/com/Assignments2/FirstCharacter.java
@@ -0,0 +1,10 @@
+package com.Assignments2;
+
+public class FirstCharacter {
+
+	public static void main(String[] args) {
+		String s = "Avinash";
+		char Avinash = s.charAt(0);
+		System.out.println("The first character of " + s + " is " + Avinash);
+	}
+}
