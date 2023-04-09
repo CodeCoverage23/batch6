acf221807417ac5fccfe46e4ca1356fc5bb57987
User <User@DESKTOP-OQ2KTH4>
2023-04-09 22:24:04 +0530

    1st letter on consol

---
+++ code_batch6/src/com/assignment2/First_letter_on_consol.java
@@ -0,0 +1,11 @@
+package com.assignment2;
+
+public class First_letter_on_consol {
+
+	public static void main(String[] args) {
+		String s="tanvi";
+		char firstChar=s.charAt(0);
+		System.out.println(firstChar);	
+	}
+
+}
