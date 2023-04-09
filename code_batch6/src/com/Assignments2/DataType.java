8e544b19008b00066165a80ab69f6160d17d0ffc
lenovo <lenovo@DESKTOP-PF8O34S>
2023-04-09 22:27:38 +0530

    primitive data type

--- code_batch6/src/com/Assignments2/DataType.java
+++ code_batch6/src/com/Assignments2/DataType.java
@@ -2,21 +2,14 @@ package com.Assignments2;
 
 public class DataType {
 	static boolean b;
-	static short s;
-	static long l;
 	static float f;
-	static double d;
 	static char c;
 	static int i;
-	static byte x;
 
 	public static void main(String[] args) {
 
 		System.out.println(b);
-		System.out.println(s);
-		System.out.println(l);
 		System.out.println(f);
-		System.out.println(d);
 		System.out.println(c);
 		System.out.println(i);
 	}
