eb5a4cadcf4cd3bd654a0f3c37bc706beca47f38
lenovo <lenovo@DESKTOP-PF8O34S>
2023-04-09 18:20:15 +0530

    GlobalVariable

---
+++ code_batch6/src/com/Assignments2/GlobalVariable.java
@@ -0,0 +1,16 @@
+package com.Assignments2;
+
+public class GlobalVariable {
+	int y;
+	int a=25;
+
+	public static void main(String[] args) {
+		GlobalVariable s = new GlobalVariable();
+		GlobalVariable s1 = new GlobalVariable();
+		System.out.println("the global variable Y is " +s. y);
+		System.out.println(s1.a);
+		// TODO Auto-generated method stub
+
+	}
+
+}
