6832f74399e73a6aa7689032b9f999dab90a05ba
DELL E7480 <DELL E7480@DESKTOP-G00PBVD>
2023-04-09 22:32:16 +0530

    GlobalVariable1

---
+++ code_batch6/src/com/basicAssignments/GlobalVariable1.java
@@ -0,0 +1,12 @@
+package com.basicAssignments;
+
+public class GlobalVariable1 {
+	int y;
+
+	public static void main(String[] args) {
+		GlobalVariable1 s = new GlobalVariable1();
+		System.out.println("the global variable Y is " +s. y);
+
+	}	
+
+	}
