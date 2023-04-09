72291d8b20b9636a73654dcd9d4eb78cbaf02541
DELL E7480 <DELL E7480@DESKTOP-G00PBVD>
2023-04-09 22:07:09 +0530

    LocalVariable

---
+++ code_batch6/src/com/basicAssignments/LocalVariable.java
@@ -0,0 +1,13 @@
+package com.basicAssignments;
+
+public class LocalVariable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=5;
+		System.out.println("LocalVariable x is"+x);
+
+	}
+
+	}
+
