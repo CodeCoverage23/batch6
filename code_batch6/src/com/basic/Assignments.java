f93ba890d7437b386a3bef73c579c6cec9f22416
DELL E7480 <DELL E7480@DESKTOP-G00PBVD>
2023-04-09 21:47:47 +0530

    Assignments

---
+++ code_batch6/src/com/basic/Assignments.java
@@ -0,0 +1,10 @@
+package com.basic;
+
+public class Assignments {
+	public static void main(String[] args) {
+        String str = "kartik";
+        char firstChar = str.charAt(0);
+        System.out.println("First Character of given string is : " + firstChar);
+    }
+}
+
