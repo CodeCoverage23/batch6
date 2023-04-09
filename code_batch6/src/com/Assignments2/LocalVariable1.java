313635130e3e8efb0f4c48fe7221d6aee663af17
lenovo <lenovo@DESKTOP-PF8O34S>
2023-04-09 18:21:27 +0530

    local variable

---
+++ code_batch6/src/com/Assignments2/LocalVariable1.java
@@ -0,0 +1,13 @@
+package com.Assignments2;
+
+public class LocalVariable1 {
+
+	public static void main(String[] args) {
+		int a=50;
+		int x=5;
+		System.out.println(a);
+		System.out.println("LocalVariable x is"+x);
+
+	}
+
+}
