d53d3e94680c1bca37a8b8b53af967baf04c32f5
DELL E7480 <DELL E7480@DESKTOP-G00PBVD>
2023-04-09 22:45:48 +0530

    DataTypesPrimitive

---
+++ code_batch6/src/com/basicAssignments/DataTypesPrimitive.java
@@ -0,0 +1,23 @@
+package com.basicAssignments;
+
+public class DataTypesPrimitive {
+	static boolean b;
+	static short s;
+	static long l;
+	static float f;
+	static double d;
+	static char c;
+	static int i;
+	static byte x;
+	public static void main(String[] args) {
+
+		System.out.println(b);
+		System.out.println(s);
+		System.out.println(l);
+		System.out.println(f);
+		System.out.println(d);
+		System.out.println(c);
+		System.out.println(i);
+
+	}
+}
