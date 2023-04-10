5036a9bed415c32823012932429ccbe9b18d73aa
Hemraj <Hemraj@DESKTOP-U0HKUMH>
2023-04-10 11:05:47 +0530

    Prim.Data

---
+++ demo/src/com/Assignments2/PrimitiveData.java
@@ -0,0 +1,26 @@
+package com.Assignments2;
+
+public class PrimitiveData {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    boolean b = false;
+    
+    byte a =87;
+    long l =87;
+    int i =34;
+    double d =0.45d;
+    short s = 76;
+    float f =0.54f;
+    char h = 'k';
+    System.out.println(a);
+    System.out.println(l);
+    System.out.println(i);
+    System.out.println(d);
+    System.out.println(s);
+    System.out.println(f);
+    System.out.println(h);
+    System.out.println(b);
+	}
+
+}
