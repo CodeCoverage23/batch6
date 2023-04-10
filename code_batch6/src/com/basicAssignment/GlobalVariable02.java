2e26db682b30fb8c08f1115ea112b6fbe9db6764
GALAXY <GALAXY@DESKTOP-OBKU59L.w.mifi>
2023-04-10 19:12:49 +0530

    GlobalY

---
+++ code_batch6/src/com/basicAssignment/GlobalVariable02.java
@@ -0,0 +1,13 @@
+package com.basicAssignment;
+
+public class GlobalVariable02 {
+
+	int y ;
+	
+	public static void main(String[]args) {
+		GlobalVariable s1 = new GlobalVariable();
+	System.out.println( "Y equal to"+25);
+	
+	
+}
+}
