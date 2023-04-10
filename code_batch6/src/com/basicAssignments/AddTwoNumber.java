44e5615251a01e81361be73a79bd96e4e9c91342
DELL E7480 <DELL E7480@DESKTOP-G00PBVD>
2023-04-10 09:08:40 +0530

    AddTwoNumber

---
+++ code_batch6/src/com/basicAssignments/AddTwoNumber.java
@@ -0,0 +1,13 @@
+package com.basicAssignments;
+public class AddTwoNumber {
+	public int sum(int a, int b) {
+		int c = a + b;
+		return c;
+	}
+	 public static void main(String[] args) {
+		 AddTwoNumber s = new AddTwoNumber();
+
+	  
+	        System.out.println(s.sum(10, 10));
+	   }
+}
