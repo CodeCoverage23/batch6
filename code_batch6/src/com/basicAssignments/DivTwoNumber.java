cb2bda65942b7e2a9f396c0679b498fa2192394d
DELL E7480 <DELL E7480@DESKTOP-G00PBVD>
2023-04-10 09:42:21 +0530

    DivTwoNumber

---
+++ code_batch6/src/com/basicAssignments/DivTwoNumber.java
@@ -0,0 +1,14 @@
+package com.basicAssignments;
+
+public class DivTwoNumber { 
+	public int div (int a, int b) {
+		int c=a/b;
+	return c;
+	}
+
+	public static void main(String[] args) {
+		DivTwoNumber s = new DivTwoNumber();
+		System.out.println(s.div(10, 10));
+	}
+
+}
