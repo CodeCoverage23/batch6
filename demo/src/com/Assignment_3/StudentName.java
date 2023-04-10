29dfc607e8b9ffa19c5606da205aa3c4602af839
oavid Dubbeldan <Kvlokhande2001@gmail.com>
2023-04-10 08:49:13 -0700

    Assignment 3

---
+++ demo/src/com/Assignment_3/StudentName.java
@@ -0,0 +1,12 @@
+package com.Assignment_3;
+
+public class StudentName {
+	public String getstudentName(String Name) {
+		return Name;
+	
+	}
+public static void main(String[] args) {
+	StudentName s=new StudentName ();
+	System.out.println(s.getstudentName("Kiran"));
+}
+}
