4764dd1a73a3e0b4f095eb1d8044626b2cf0475a
user <user@LAPTOP-9K2SO253>
2023-04-10 11:21:55 +0530

    Assignment

---
+++ code_batch6/src/com/assignment2/FirstCharacter.java
@@ -0,0 +1,12 @@
+package com.assignment2;
+
+public class FirstCharacter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   String str = "Rushi";
+   char firstchar = str.charAt(0);
+   System.out.println("firstchar of given string is : " + firstchar);
+	}
+
+}
