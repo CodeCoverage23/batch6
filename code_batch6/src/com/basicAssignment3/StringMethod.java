d5e618e7ebd40d1163b19b2307974e25b9ce2075
GALAXY <GALAXY@DESKTOP-OBKU59L.w.mifi>
2023-04-10 19:14:23 +0530

    String

---
+++ code_batch6/src/com/basicAssignment3/StringMethod.java
@@ -0,0 +1,16 @@
+package com.basicAssignment3;
+
+public class StringMethod {
+
+	public String getstudentname(String name) {
+		return name;
+			}
+	
+	public static void main(String[] args) {
+	
+		StringMethod  s1       =  new StringMethod ();
+		System.out.println(s1.getstudentname ("Sarthak")   );
+	
+	
+}
+}
