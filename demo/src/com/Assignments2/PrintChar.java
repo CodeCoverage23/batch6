3f81738fc6e1339c95dda4f316ce297fe3bba16f
Hemraj <Hemraj@DESKTOP-U0HKUMH>
2023-04-10 11:22:14 +0530

    printCh

---
+++ demo/src/com/Assignments2/PrintChar.java
@@ -0,0 +1,12 @@
+package com.Assignments2;
+
+public class PrintChar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         String st = "Sweety";
+         char name= st.charAt(0);
+         System.out.println(name);
+	}
+
+}
