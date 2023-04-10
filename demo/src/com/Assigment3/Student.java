ce4d3551ee87a8a4960cd38c30fba2d3415989e6
Hemraj <Hemraj@DESKTOP-U0HKUMH>
2023-04-10 11:07:51 +0530

    GetStudent

---
+++ demo/src/com/Assigment3/Student.java
@@ -0,0 +1,17 @@
+package com.Assigment3;
+
+public class Student {
+	 String student(String f) {
+		   
+//		String  name;
+		   return f;
+	 }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      Student st = new Student();
+      String s = st.student("Gargi");
+      System.out.println(s);
+	}
+
+}
