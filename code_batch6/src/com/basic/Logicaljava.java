659b40db248a3d4a8eb84655ebd021f29a746cab
user <user@LAPTOP-9K2SO253>
2023-04-10 11:24:18 +0530

    Assignment

---
+++ code_batch6/src/com/basic/Logicaljava.java
@@ -0,0 +1,16 @@
+package com.basic;
+
+public class Logicaljava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=20;
+		int c=30;
+		System.out.println((++a>b) && (++a>b));
+		System.out.println(a);
+		System.out.println("He is a java Developerpor");
+
+	}
+
+}
