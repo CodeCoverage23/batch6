2db8717dcebdaf4fb3d9c1e4dd5a3bb3695b1f92
hp <hp@DESKTOP-7OTSBFL>
2023-04-10 09:20:01 +0530

    Assignment_3

---
+++ demo/src/com/Assignment_3/Student.java
@@ -0,0 +1,15 @@
+package com.Assignment_3;
+
+public class Student {
+	public String getstudentname(String name) {
+		return name;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student s = new Student();
+		System.out.println("String getstudentname(Bhavika)");
+
+	}
+
+}
