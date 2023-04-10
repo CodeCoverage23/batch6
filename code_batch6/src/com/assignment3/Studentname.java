892d28ac8d3102b20c674741041789490b156703
user <user@LAPTOP-9K2SO253>
2023-04-10 15:24:12 +0530

    Assignment3

---
+++ code_batch6/src/com/assignment3/Studentname.java
@@ -0,0 +1,13 @@
+package com.assignment3;
+
+public class Studentname {
+	
+	public static void main(String[] args) {
+
+		String name  = new String("rushi");
+System.out.println(name);
+		
+
+	}
+
+}
