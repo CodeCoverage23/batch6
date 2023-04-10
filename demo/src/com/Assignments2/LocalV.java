f7cd0e84e4eac73b61bfc023a50b6e45a4ce3d31
Hemraj <Hemraj@DESKTOP-U0HKUMH>
2023-04-10 14:48:42 +0530

    Local

---
+++ demo/src/com/Assignments2/LocalV.java
@@ -0,0 +1,12 @@
+package com.Assignments2;
+
+public class LocalV {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+             int x=50;
+//             LocalV  lv = new LocalV();
+             System.out.println(x);
+	}
+
+}
