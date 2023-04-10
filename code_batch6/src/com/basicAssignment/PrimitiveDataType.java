eb81b4ac327a67098ef1bea0715a0390d9781de9
GALAXY <GALAXY@DESKTOP-OBKU59L.w.mifi>
2023-04-10 19:15:47 +0530

    DataType

---
+++ code_batch6/src/com/basicAssignment/PrimitiveDataType.java
@@ -0,0 +1,29 @@
+package com.basicAssignment;
+
+
+
+public class PrimitiveDataType {
+	static int num=10;
+	static	double b= 19.99d;  
+	static short d = 50;  
+	static char myChar= 'S'; 
+	static   long range =1763456;
+	static   short temparture =-2;
+	static	boolean flag=true;
+	static	float c=12.45f;
+ 
+	
+	public static void main(String[] args) {
+	
+		
+	  
+	System.out.println(num);  
+	System.out.println(b);   
+	System.out.println(d);
+	System.out.println(myChar);
+	System.out.println(range);
+	System.out.print(temparture);
+	System.out.println(flag);
+    System.out.println(c);
+	}
+}
