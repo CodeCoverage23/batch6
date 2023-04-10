8d9799339b8667d51e21b1ee9a047cd948138f5b
GALAXY <GALAXY@DESKTOP-OBKU59L.w.mifi>
2023-04-10 19:15:08 +0530

    local

---
+++ code_batch6/src/com/basicAssignment/LocalVariable.java
@@ -0,0 +1,19 @@
+package com.basicAssignment;
+
+public class LocalVariable {
+
+	
+	public static void main(String[] args) {
+
+		int a=50;
+		int x=9;
+		System.out.println(a);
+		System.out.println("LocalVariable x is"+x);
+
+	}
+
+
+	
+	
+	}
+
