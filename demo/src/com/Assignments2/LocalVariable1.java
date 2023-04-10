22008d29e67e4413c9bcff54e4c3f8472e7828be
Hemraj <Hemraj@DESKTOP-U0HKUMH>
2023-04-10 11:14:11 +0530

    LocalV1

---
+++ demo/src/com/Assignments2/LocalVariable1.java
@@ -0,0 +1,13 @@
+package com.Assignments2;
+
+public class LocalVariable1 {
+
+	public static void main(String[] args) {
+		int x=2;
+		LocalVariable1 l = new LocalVariable1();
+          
+	System.out.println(x);
+		
+	}
+
+}
