f39eeab5ce6c308cec8bf397f0d38c417601cf87
admin <admin@DESKTOP-5S64ACI>
2023-04-10 11:04:21 +0530

    Assignment 3

---
+++ demo/src/com/assignment4/Student.java
@@ -0,0 +1,16 @@
+package com.assignment4;
+
+
+public class Student {
+	public String getStudentName(String name) {
+		return name;
+		
+	}
+	public static void main(String[]args) {
+		Student s = new Student();
+		System.out.println(s.getStudentName("Gauri"));
+				
+	}
+}
+
+
