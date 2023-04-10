5865017b99669d426443460e3b10eee257a1ee61
Hemraj <Hemraj@DESKTOP-U0HKUMH>
2023-04-10 11:02:57 +0530

    Global

---
+++ demo/src/com/Assignments2/GlobalV.java
@@ -0,0 +1,17 @@
+package com.Assignments2;
+
+public class GlobalV {
+	 int y=1;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       GlobalV g = new GlobalV();
+//       int y2 = g.y; 
+       System.out.println(g.y);
+             
+      
+      
+       
+	}
+
+}
