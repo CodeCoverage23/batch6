490ee952cdacec65109bb77f0749c980d1fce53a
Hemraj <Hemraj@DESKTOP-U0HKUMH>
2023-04-10 11:01:01 +0530

    Commit

---
+++ demo/src/com/Assignments2/GlobalV2.java
@@ -0,0 +1,13 @@
+package com.Assignments2;
+
+public class GlobalV2 {
+     int y = 25;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		GlobalV2 gv = new GlobalV2();
+//		gv.y;
+		System.out.println(gv.y);
+		
+	}
+
+}
