3040b1cb4a223f22b4fdaf3867d1739da5af6118
Hemraj <Hemraj@DESKTOP-U0HKUMH>
2023-04-10 12:01:30 +0530

    Per

---
+++ demo/src/com/Assignment/Percentage.java
@@ -0,0 +1,25 @@
+package com.Assignment;
+
+public class Percentage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         int  Math = 78;
+         int  english = 69;
+         int  Hindi = 90;
+         int   java =66;
+         int    physics=54;
+         int total = Math+english+Hindi+java+physics;
+         double per = total/ 5.0;
+         Percentage p = new  Percentage ();
+          System.out.println("Marke1: "+Math);
+          System.out.println("Marks2:"+english);
+          System.out.println("Marks3:"+Hindi);
+          System.out.println("Marks4:"+java);
+          System.out.println("Marks4:"+physics);
+          System.out.println("Total :" +total);
+          System.out.println("Percentage :"+per);
+          
+	}
+
+}
