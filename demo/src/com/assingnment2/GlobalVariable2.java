24b3960eef669f5673be37ea444f9089411e129b
dell <dell@DESKTOP-I2B4679>
2023-04-10 07:46:00 +0530

    Assignment No.2

---
+++ demo/src/com/assingnment2/GlobalVariable2.java
@@ -0,0 +1,15 @@
+package com.assingnment2;
+
+public class GlobalVariable2 {
+	
+	int a = 25;
+
+	public static void main(String[] args) {
+		
+		GlobalVariable2 g = new GlobalVariable2();
+		System.out.println("Global Variable :" + g.a);
+		
+
+	}
+
+}
