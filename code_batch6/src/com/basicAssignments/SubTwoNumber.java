de772b230982a2b7673385db0b03dbd0b42ea2a4
DELL E7480 <DELL E7480@DESKTOP-G00PBVD>
2023-04-10 09:24:45 +0530

    SubTwoNumber

---
+++ code_batch6/src/com/basicAssignments/SubTwoNumber.java
@@ -0,0 +1,17 @@
+package com.basicAssignments;
+
+public class SubTwoNumber {
+	public int sub (int a ,int b) {
+		int c = a-b;
+		return c;
+	}
+
+	public static void main(String[] args) {
+		SubTwoNumber s = new SubTwoNumber();
+		System.out.println(s.sub(10,10));
+
+		
+
+	}
+
+}
