875d6d85bd49eda0d032db051dcd0b38eccffe2c
DELL E7480 <DELL E7480@DESKTOP-G00PBVD>
2023-04-10 09:32:25 +0530

    MulTwoNumber

---
+++ code_batch6/src/com/basicAssignments/MulTwoNumber.java
@@ -0,0 +1,16 @@
+package com.basicAssignments;
+
+public class MulTwoNumber {
+	public int mul (int a,int b) {
+		int c=a*b;
+		return c;
+	}
+
+	public static void main(String[] args) {
+		MulTwoNumber s = new MulTwoNumber();
+		System.out.println(s.mul(10,10));
+		
+
+	}
+
+}
