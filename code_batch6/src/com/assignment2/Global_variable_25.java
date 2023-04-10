55ba2823baeb4b2abaf865aa5df1c9d90aa4ae0f
User <User@DESKTOP-OQ2KTH4>
2023-04-10 19:52:29 +0530

    global as 25

---
+++ code_batch6/src/com/assignment2/Global_variable_25.java
@@ -0,0 +1,15 @@
+package com.assignment2;
+
+public class Global_variable_25 {
+
+		static int a=5;
+		static int b=5;
+		static int c=a*b;
+
+		public static void main(String[] args) {
+
+			System.out.println(c);
+
+
+}
+	}
