f5221b8d231fdc23ef4f393b340fd7661ab1087b
Hemraj <Hemraj@DESKTOP-U0HKUMH>
2023-04-11 16:52:49 +0530

    swapping

---
+++ demo/src/com/Assignment5/SwapNumber.java
@@ -0,0 +1,18 @@
+package com.Assignment5;
+
+public class SwapNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         int s  = 80;
+         int h =   55;
+         int k =s;
+         System.out.println("before swap :"+s+" "+h);
+         s=h;
+         h=k;
+         
+         
+         System.out.println("after swap:"+s+" "+h);
+	}
+
+}
