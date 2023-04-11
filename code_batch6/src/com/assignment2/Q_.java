9ac66ba1a24ddeeb61983f2df3a7964001af1977
SurajDotNetDeveloper <surajcool9623@gmail.com>
2023-04-11 09:37:46 +0530

    Assignment 3

---
+++ code_batch6/src/com/assignment2/Q_.java
@@ -0,0 +1,17 @@
+package com.assignment2;
+
+//Create method which name as getStudentName
+//which return student name
+public class Q_ {
+	public String getStudentName(String name) {
+
+		return name;
+
+	}
+
+	public static void main(String[] args) {
+		Q_ sc = new Q_();
+		String result = sc.getStudentName("BHOLENATH BABA");
+		System.out.println(result);
+	}
+}
