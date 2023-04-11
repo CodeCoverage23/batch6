42f932364ae4800730c31631f4eb01e35f8b9f38
GALAXY <GALAXY@DESKTOP-OBKU59L.w.mifi>
2023-04-11 17:22:34 +0530

    squareroot

---
+++ code_batch6/src/com/basicAssignment5/SquareRoot.java
@@ -0,0 +1,22 @@
+package com.basicAssignment5;
+import java.util.Scanner;
+public class SquareRoot {
+
+	
+		
+	  public static void main(String[] args) {
+		  System.out.println("Enter the number :");
+		    Scanner sc= new Scanner(System.in);
+	         int n=sc.nextInt();
+	    
+	    // for loop  
+	    for (int i =1;i <= 1; ++i) {
+	      System.out.println(n *n);
+	    }
+	  }
+	}
+	
+	
+	
+	
+
