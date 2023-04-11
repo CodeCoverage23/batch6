dee5eff8fd11e9422e990fb047498a713c1f7b6c
prade <prade@DESKTOP-NS5DLR9>
2023-04-11 09:25:15 +0530

    String program

---
+++ demo/src/com/demo/StringComparison.java
@@ -0,0 +1,29 @@
+package com.demo;
+
+public class StringComparison {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 1-way -> refference comparison
+		String s="Pradeep";
+		String s1="patankar";
+		String s6="patankar";
+		String s3="PRADEEP";
+		String s5="pradeep";
+//	System.out.println(s==s1);	//false
+	//2-way-> Content comparison
+	String s2="pradeep";
+	String S4=new String("pradeep");
+	System.out.println(s.equals(s3));//true
+			
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ demo/src/com/demo/StringDemo.java
@@ -0,0 +1,19 @@
+package com.demo;
+
+public class StringDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//char[] ch= {'S','t','r','i','n','g'};
+//
+//String s=new String(ch);
+//System.out.println(s);
+//	String s="pradeep";
+//		s.concat("patankar");
+	String s1="pradeep"+" Patankar";
+//		String s1=new String("pradeep");
+		System.out.println(s1);
+		
+	}
+
+}
