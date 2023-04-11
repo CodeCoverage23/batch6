7a9fda2751a5319bbeaab22a85216e5bc42bd416
GALAXY <GALAXY@DESKTOP-OBKU59L.w.mifi>
2023-04-11 18:08:23 +0530

    season

---
+++ code_batch6/src/com/basicAssignment5/Season02.java
@@ -0,0 +1,54 @@
+package com.basicAssignment5;
+import java.util.Scanner;
+public class Season02{
+ public static void main(String args[]){
+    String season;
+    Scanner sc = new Scanner (System.in);
+   System.out.println("Enter Any  number");
+    int entry = sc.nextInt();
+      switch (entry)
+        {
+            case 12:System.out.println("December");
+            season = "Winter";
+           break;
+           
+            case 1:System.out.println("january");
+            season = "Winter";
+            break;
+            case 2:System.out.println("febraury");
+            season = "Winter";
+            break;
+            case 3:System.out.println("march");
+            season = "Spring";
+            break;
+            case 4:System.out.println("april");
+            season = "Spring";
+            break;
+            case 5:System.out.println("may");
+            season = "Spring";
+            break;
+            case 6:System.out.println("june");
+            season = "Summer";          
+            break;
+            case 7:System.out.println("july");
+            season = "Summer";
+            break;
+            case 8:System.out.println("august");
+            season = "Summer";
+            break;
+            case 9:System.out.println("september");
+            season = "Autumn";
+            break;
+            case 10:System.out.println("octomber");
+            season = "Autumn";
+            break;
+            case 11:System.out.println("november");
+            season = "Autumn";
+            break;
+            default:
+            season = "Invalid Month";
+       }
+        System.out.println("The entered month is in the " +season + ".");
+   }
+ }
+
