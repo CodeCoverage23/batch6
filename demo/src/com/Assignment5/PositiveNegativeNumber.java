b2f6724cd1c5c50eab88df73e2800c3369c595b3
Hemraj <Hemraj@DESKTOP-U0HKUMH>
2023-04-11 16:12:36 +0530

    TwoNumber

---
+++ demo/src/com/Assignment5/PositiveNegativeNumber.java
@@ -0,0 +1,28 @@
+package com.Assignment5;
+
+import java.util.Scanner;
+
+public class PositiveNegativeNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+  Scanner sc= new Scanner(System.in);
+		 int num= sc.nextInt();
+  System.out.println("Enter number");
+  
+  if(num>0) {
+	  System.out.println("number is positive");
+  }
+  else if(num<0 ) {
+	  System.out.println("number is negative");
+  }
+  
+	
+	else {
+		System.out.println("not number");
+	}
+
+	}
+}
+
