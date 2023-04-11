1a98d34fbd0d3c1a49d0fe31cf60f2d59a071a20
GALAXY <GALAXY@DESKTOP-OBKU59L.w.mifi>
2023-04-11 16:44:33 +0530

    positive and negative

--- code_batch6/src/com/basicAssignment5/NumberPositiveNegative.java
+++ code_batch6/src/com/basicAssignment5/NumberPositiveNegative.java
@@ -11,15 +11,15 @@ public class NumberPositiveNegative {
 		System.out.println("enter value");
 		int number = sc.nextInt();
 	  
-	  // true if number is less than 0
+	
     if (number < 0.0)
         System.out.println(number + " is a negative number.");
 
-    // true if number is greater than 0
+   
     else if ( number > 0.0)
         System.out.println(number + " is a positive number.");
 
-    // if both test expression is evaluated to false
+   
     else
         System.out.println(number + " is 0.");
 }
