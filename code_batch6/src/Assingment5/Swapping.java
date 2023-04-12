998de058c3509c3e786a7d8bc86610a37cf9e36e
HP <HP@DESKTOP-9GEHNH3>
2023-04-12 22:06:51 +0530

    Java code to swapp two number

---
+++ code_batch6/src/Assingment5/Swapping.java
@@ -0,0 +1,13 @@
+package Assingment5;
+
+public class Swapping {
+
+	public static void main(String[] args) {
+		int a = 20, b = 30, c;
+		c=a;
+		a=b;
+		b=c;
+		System.out.println("a= "+a+"b= "+ b);
+	}
+
+}
