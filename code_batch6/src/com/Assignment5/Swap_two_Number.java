499ed7960569bdcfdfff847c4b88421b42f7ceb2
user <user@LAPTOP-9K2SO253>
2023-04-12 18:17:56 +0530

    Swap

---
+++ code_batch6/src/com/Assignment5/Swap_two_Number.java
@@ -0,0 +1,18 @@
+package com.Assignment5;
+
+public class Swap_two_Number {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    int a = 30;
+    int b = 40;
+    System.out.println("Before Swap: a = " +a);
+    System.out.println("Before Swap: b = " +b);
+    int c=a; a=b; b=c;
+    System.out.println("After Swap: a = " +a);
+    System.out.println("After Swap: b = " +b);
+    
+    
+	}
+
+}
