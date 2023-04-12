fd9c4306d15aff8b2c6c3b03ec5a3c1e753e17e4
user <user@LAPTOP-9K2SO253>
2023-04-12 18:07:27 +0530

    Number

---
+++ code_batch6/src/com/Assignment5/Positive_Nigative.java
@@ -0,0 +1,24 @@
+package com.Assignment5;
+   import java.util.Scanner; 
+public class Positive_Nigative {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     Scanner sc = new Scanner(System.in);
+     
+     System.out.println("Enter a number: ");
+	int number = sc.nextInt();
+	if(number>=0) {
+		System.out.println("Positive Number");
+		
+	}
+	else {
+		System.out.println("Nigative Number");
+		
+	}
+	
+	
+	}
+	
+
+}
