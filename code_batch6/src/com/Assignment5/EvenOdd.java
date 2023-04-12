1b33db7526a89907edcb3f3bd77de2f0cca1245d
lenovo <lenovo@DESKTOP-PF8O34S>
2023-04-12 19:07:38 +0530

    Even Odd Program

---
+++ code_batch6/src/com/Assignment5/EvenOdd.java
@@ -0,0 +1,21 @@
+package com.Assignment5;
+
+import java.util.Scanner;
+
+public class EvenOdd{
+	// int a=7;
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter the number");
+		int n = s.nextInt();
+		int a = 0;
+
+		if (a % 2 == 0) {
+			System.out.println("number is even ");
+		} else {
+			System.out.println("number is odd ");
+		}
+	}
+
+}
