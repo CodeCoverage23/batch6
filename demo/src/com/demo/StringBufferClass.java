2ff38aacea507be2f31068fd33beb0ffb194c813
prade <prade@DESKTOP-NS5DLR9>
2023-04-12 09:19:20 +0530

    subString and Stringbuffer

---
+++ demo/src/com/demo/StringBufferClass.java
@@ -0,0 +1,18 @@
+package com.demo;
+
+public class StringBufferClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	StringBuffer sb = new StringBuffer("java");
+	sb.append("tpoint");
+		System.out.println(sb);
+	String s=new String("java");
+	String s1=s.concat("tpoint");
+	System.out.println(s);
+	System.out.println(s1);
+		
+	}
+
+}
---
+++ demo/src/com/demo/StringSub.java
@@ -0,0 +1,19 @@
+package com.demo;
+
+public class StringSub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String s="javatpoint";
+		
+		System.out.println(s.substring(0,6));
+		System.out.println(s.length());
+		System.out.println(s.charAt(8));
+		System.out.println(s.indexOf("a"));
+		System.out.println(s.contains("hello"));
+		
+		
+	}
+
+}
