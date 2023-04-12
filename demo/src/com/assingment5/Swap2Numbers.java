dde4c975730811e6f1b58d09e5abd1adf88c666b
mansi <mansi@ASUS>
2023-04-12 16:38:23 +0530

    swap number assingment 5 update

---
+++ demo/src/com/assingment5/Swap2Numbers.java
@@ -0,0 +1,22 @@
+package com.assingment5;
+
+import java.util.Scanner;
+
+//8. Write a program to swap the two numbers.
+public class Swap2Numbers {
+	public static void main(String[] args) {
+		System.out.print("Enter a two numbers");
+		Scanner sc = new Scanner(System.in);
+		double a = sc.nextDouble();
+		double b = sc.nextDouble();
+		double c;
+		
+		c=a;
+		a=b;
+		b=c;
+		
+		System.out.println(a + " "+ b);
+	
+		
+	}
+}
