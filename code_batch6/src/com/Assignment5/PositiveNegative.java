f1ed0a6b50008636d9101d64620e40ea66735318
lenovo <lenovo@DESKTOP-PF8O34S>
2023-04-12 17:58:26 +0530

    PositiveNegative program

---
+++ code_batch6/src/com/Assignment5/PositiveNegative.java
@@ -0,0 +1,21 @@
+package com.Assignment5;
+
+import java.util.Scanner;
+
+public class PositiveNegative {
+public static void main(String[] args) {
+	Scanner p = new Scanner(System.in);
+	System.out.println("Enter the number ");
+	int n=p.nextInt();
+	if(n>0) {
+		System.out.println("Number is positive");
+	}
+	
+	else if(n<0){
+		System.out.println("Number is negative");
+	}
+	else {
+		System.out.println("Number is zero");
+	}
+}
+}
