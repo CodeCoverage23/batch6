fb7c98fa9f8c8d31f7d80bb8b9a36861300acda6
user <user@LAPTOP-9K2SO253>
2023-04-12 18:25:46 +0530

    Square Number

---
+++ code_batch6/src/com/Assignment5/Square_Number.java
@@ -0,0 +1,18 @@
+package com.Assignment5;
+
+public class Square_Number {
+
+
+     int a = 12;
+     int b = a*a;
+
+  
+     public static void main(String[] args) {
+ 		// TODO Auto-generated method stub
+	Square_Number s = new Square_Number();
+	System.out.println("Square Number : " + s.b);
+     
+     
+     }
+
+}
