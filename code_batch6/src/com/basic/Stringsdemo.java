4c6a8b8f05d2bfb11e52a3937239badb564a5d82
user <user@LAPTOP-9K2SO253>
2023-04-12 13:05:32 +0530

    String

---
+++ code_batch6/src/com/basic/Stringsdemo.java
@@ -0,0 +1,25 @@
+package com.basic;
+
+public class Stringsdemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ 
+		String s = "Rushi";
+		String s1="Pande";
+		String s2="Ajinkay";
+		String s3="Tajane";
+		String s4="Sakhre";
+		 System.out.println("s==s1");
+		String s7=new String("Rushikesh");
+		String s6="Anup";
+		String s5=new String("Wankhade");
+		 
+		 System.out.println(s.equals(s3));
+		
+
+	}
+	
+	
+
+}
