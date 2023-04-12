f67c4716f007e0b07ec9bcce875a4f356e46bf43
HP <HP@DESKTOP-9GEHNH3>
2023-04-12 21:56:22 +0530

    Java code to calculate square of number:

---
+++ code_batch6/src/Assingment5/SquareOfNumber.java
@@ -0,0 +1,18 @@
+package Assingment5;
+import java .util.Scanner;
+
+public class SquareOfNumber {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num;
+		System.out.println("Enter number to calculate square");
+		num = sc.nextInt();
+		long Square;
+		Square = num*num;
+		System.out.println("Square of number is="+ Square);
+		
+
+	}
+
+}
