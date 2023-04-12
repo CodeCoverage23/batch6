f7d622b90f9253cc562be009131f97975f2acec0
User <User@DESKTOP-OQ2KTH4>
2023-04-12 23:08:14 +0530

    greter than 100 assignment5

--- code_batch6/src/com/assignment5/Greterthan_100.java
+++ code_batch6/src/com/assignment5/Greterthan_100.java
@@ -4,20 +4,19 @@ import java.util.*;
 
 public class Greterthan_100 {
 	
-	public static void main(String[] args) {
-		 
-		Scanner sc=new Scanner(System.in);
-		System.out.println("enter number");
-		int number=sc.nextInt();
-		if(number>100) {
-			System.out.println("number is greter than 100");
-		}
-		else if(number >=100) {
-			System.out.println("number is 100");
-		}
-		else {
-			System.out.println("number is less than 100");
-		}
-		}
-	}
-
+public static void main(String[] args) {
+	
+	Scanner sc= new Scanner(System.in);
+    System.out.println("enter number");
+    int number=sc.nextInt();
+    if(number>100) {
+    	System.out.println("number is greter than 100" );
+    }
+    else if (number>100){
+    	System.out.println("number is less than 100");
+    }
+    else{
+    	System.out.println("number is 100");
+    }
+}
+}
