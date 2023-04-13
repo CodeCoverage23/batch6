20b509cb280c0ae66b8974fabd277d96e653b59a
DELL <DELL@DESKTOP-SNUQ9IK>
2023-04-13 13:39:29 +0530

    Assignment3

---
+++ demo/src/com/Assignment3/Assignment1.java
@@ -0,0 +1,19 @@
+package com.Assignment3;
+
+public class Assignment1 {
+  static String getStudentName(String name) {
+  return name;
+}
+public static void main(String[] args) { 
+	Assignment1 a =new Assignment1();
+	String s = Assignment1.getStudentName("vaishnavi");
+	System.out.println("student name is ="+ s);
+	
+	
+			
+			
+	
+	
+	
+}
+}
