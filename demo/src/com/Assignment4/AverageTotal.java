3f70b46d22a0a19a2100f455b408e15bd0b5164a
DELL <DELL@DESKTOP-SNUQ9IK>
2023-04-13 13:40:04 +0530

    Assignment4

---
+++ demo/src/com/Assignment4/AverageTotal.java
@@ -0,0 +1,19 @@
+package com.Assignment4;
+
+public class AverageTotal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int math = 65;
+int sci = 76;
+int eng = 68;
+int phy = 78;
+int bio = 62;
+ 
+int total = math+sci+eng+phy+bio;
+int average = total/5;
+System.out.println(total);
+System.out.println(average);
+	}
+
+}
