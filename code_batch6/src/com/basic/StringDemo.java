a2dfd7125587cdd0e0460be91364709359fbc804
user <user@LAPTOP-9K2SO253>
2023-04-14 16:14:19 +0530

    Demo

---
+++ code_batch6/src/com/basic/StringDemo.java
@@ -0,0 +1,20 @@
+package com.basic;
+
+public class StringDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     String s = "Eclipse_IDE";
+     System.out.println(s.substring(0,8));
+     System.out.println(s.length());
+     System.out.println(s.charAt(10));
+     System.out.println(s.indexOf("e"));
+     System.out.println(s.contains("Eclipse"));
+     
+		
+		
+		
+		
+	}
+
+}
