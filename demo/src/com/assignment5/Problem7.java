2fa75f41c4ddde5dbbb59841b10a8f9f0b3cd4e4
User <User@DESKTOP-OQ2KTH4>
2023-04-14 18:13:57 +0530

    square of a number

---
+++ demo/src/com/assignment5/Problem7.java
@@ -0,0 +1,17 @@
+package com.assignment5;
+
+import java.util.Scanner;
+
+public class Problem7 {
+	public static void main(String[] args) {
+		int num;
+		Scanner s = new Scanner(System.in);
+		System.out.println("enter the no.");
+		int a=s.nextInt();
+		num=a*a;
+		System.out.println("the square is: " +num);
+		
+		
+	}
+
+}
