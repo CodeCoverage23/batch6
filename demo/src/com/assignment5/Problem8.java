5575fe2cbe53dee620b5bed149fbcddd7218611f
User <User@DESKTOP-OQ2KTH4>
2023-04-14 18:15:10 +0530

    swipe two no.

---
+++ demo/src/com/assignment5/Problem8.java
@@ -0,0 +1,19 @@
+package com.assignment5;
+
+import java.util.Scanner;
+
+public class Problem8 {
+	public static void main(String[] args) {
+		int a=40,b=50;
+		a=a+b;
+		b=a-b;
+		a=a-b;
+		System.out.println("a="+a);
+		System.out.println("b="+b);
+		
+		
+		
+		
+	}
+
+}
