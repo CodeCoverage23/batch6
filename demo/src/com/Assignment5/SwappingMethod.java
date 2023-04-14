0eba3b784a94939ed5484301454c98ab1d47720c
Manthan Aage <Manthan Aage@LAPTOP-09BEGS6L>
2023-04-14 17:20:15 +0530

    'Assignment 5'

--- demo/src/com/Assignment5/SwappingMethod.java
+++ demo/src/com/Assignment5/SwappingMethod.java
@@ -5,12 +5,12 @@ public class SwappingMethod {
 	public static void main(String[] args) {
 		int a=35,b=18,c;
 		
-		int d=a+b;
-		int e=a-b;
-		int f=a-b;
-		System.out.println("The value of d"+d);
-		System.out.println("The value of e"+e);
-		System.out.println("The value of f"+f);
+		   a=a+b;
+		   b=a-b;
+		   a=a-b;
+		System.out.println("The value of d"+a);
+		System.out.println("The value of e"+b);
+		System.out.println("The value of f"+a);
 		
 		
 
