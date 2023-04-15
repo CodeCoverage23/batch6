1ade622bbb94fbc6bd98f4079f9b1b614ded9db2
asus <asus@LAPTOP-LN2QCEDH>
2023-04-15 15:54:59 +0530

    Even and Odd number

---
+++ code_batch6/src/com/Assignment5/EvenAndOddNumber.java
@@ -0,0 +1,18 @@
+package com.Assignment5;
+
+public class EvenAndOddNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     int num=44;
+    if(num%2==0) {
+    	System.out.println("the no is Even");
+    }
+    else {
+		System.out.println("the no is Odd");
+    }
+		
+		
+	}
+
+}
