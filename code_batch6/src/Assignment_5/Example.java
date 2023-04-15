860d92f85518f265bb94a7516fba32ad92a2cc0a
PC <PC@DESKTOP-JFV6KS0>
2023-04-15 12:44:02 +0530

    example

---
+++ code_batch6/src/Assignment_5/Example.java
@@ -0,0 +1,20 @@
+package Assignment_5;
+
+public class Example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int month = 2;
+String monthString = " ";
+switch(month) {
+case 1: monthString="1-january";
+break;
+case 2: monthString="2-february";
+break;
+
+default:System.out.println("invalid month");
+}
+System.out.println(monthString);
+	}
+
+}
