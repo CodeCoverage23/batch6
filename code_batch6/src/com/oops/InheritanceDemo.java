83ef6444e1064fe51954649d50f0fd5487e7c55d
swapn <swapn@LAPTOP-BJNVAM67>
2023-04-15 21:20:17 +0530

    Inheritance

---
+++ code_batch6/src/com/oops/InheritanceDemo.java
@@ -0,0 +1,21 @@
+package com.oops;
+
+public class InheritanceDemo extends Student {
+
+	public void studentData() {
+		System.out.println("Single Student");
+	}
+
+	public static void main(String[] args) {
+
+		InheritanceDemo d = new InheritanceDemo();
+
+		d.exam();
+		d.study();
+		d.studentData();
+
+		System.out.println(d.rollNo);
+		System.out.println(d.name);
+	}
+
+}
---
+++ code_batch6/src/com/oops/Student.java
@@ -0,0 +1,19 @@
+package com.oops;
+
+public class Student {
+
+	int rollNo = 101;
+	String name = "Virat";
+
+	public void exam() {
+		System.out.println("Pass");
+	}
+
+	public void study() {
+		System.out.println("Completed");
+	}
+
+	private void practical() {
+		System.out.println("Not completed");
+	}
+}
