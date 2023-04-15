0fce08febba12cb0d1a59b8023f64d95a62ccf76
PC <PC@DESKTOP-JFV6KS0>
2023-04-15 12:58:00 +0530

    LocalVariableAss_2

---
+++ code_batch6/src/com/basic/LocalVariableAssignment2.java
@@ -0,0 +1,12 @@
+package com.basic;
+
+public class LocalVariableAssignment2 {
+	public static int x;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int x=50;
+System.out.println(x);
+	}
+
+}
