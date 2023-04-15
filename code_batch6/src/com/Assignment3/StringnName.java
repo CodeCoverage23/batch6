5fbac1b8e9b5c54379cf417743187e997b613ffc
asus <asus@LAPTOP-LN2QCEDH>
2023-04-15 15:41:01 +0530

    String name

---
+++ code_batch6/src/com/Assignment3/StringnName.java
@@ -0,0 +1,18 @@
+package com.Assignment3;
+
+public class StringnName {
+
+	public String getStudentName(String name) {
+		return name;
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		StringnName s=new StringnName();
+		System.out.println(s.getStudentName("Ashwini"));
+		
+	}
+
+}
