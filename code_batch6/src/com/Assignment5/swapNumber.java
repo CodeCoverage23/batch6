1abcef802c29ef5512938005f17b55db6b97e8b9
asus <asus@LAPTOP-LN2QCEDH>
2023-04-15 18:23:02 +0530

    update

---
+++ code_batch6/src/com/Assignment5/swapNumber.java
@@ -0,0 +1,16 @@
+package com.Assignment5;
+
+public class swapNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  int a=20,b=40;
+  a=a+b;//20+40=60
+  b=a-b;//60-40=20
+  a=a-b;//60-20=40
+  System.out.println("a="+a+"b=" +b);
+  
+  
+	}
+
+}
