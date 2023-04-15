95edbf8a39299efed824f99aca75cf52bb04856d
asus <asus@LAPTOP-LN2QCEDH>
2023-04-15 15:47:35 +0530

    Total marks and Average Assignment 4

---
+++ code_batch6/src/com/Assignment4/TotalmarksAndAverage.java
@@ -0,0 +1,23 @@
+package com.Assignment4;
+
+public class TotalmarksAndAverage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  int science=85;
+  int math=78;
+  int english=73;
+  int history=75;
+  int cs=87;
+   int Totalmarks=science+math+english+history+cs;
+    double Average=Totalmarks/5;
+		
+		System.out.println("Totalmarks of 5 subject is: "+Totalmarks);
+		
+			System.out.println("Average of 5 subject is: "+Average);	
+		
+		
+		
+	}
+
+}
