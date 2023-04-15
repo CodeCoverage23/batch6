43d358abd29c441b3d5014e10c130304afef4035
PC <PC@DESKTOP-JFV6KS0>
2023-04-15 10:16:06 +0530

    Ass3_StudentName

---
+++ code_batch6/src/Assignment_3/Ass3_StudentName.java
@@ -0,0 +1,14 @@
+package Assignment_3;
+
+public class Ass3_StudentName {
+	
+public String getStudentName(String name) {
+	return name;
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Ass3_StudentName s = new Ass3_StudentName();
+System.out.println(s.getStudentName("Pooja"));
+	}
+
+}
