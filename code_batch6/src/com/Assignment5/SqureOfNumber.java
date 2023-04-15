68ca7667b81e61a77f45f53a1b9e72983bfe1480
asus <asus@LAPTOP-LN2QCEDH>
2023-04-15 18:24:22 +0530

    Squre of number

---
+++ code_batch6/src/com/Assignment5/SqureOfNumber.java
@@ -0,0 +1,17 @@
+package com.Assignment5;
+
+import java.util.Scanner;
+
+public class SqureOfNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ int num,a;
+ Scanner sc=new Scanner(System.in);
+ System.out.println("enter the number");
+ num=sc.nextInt();
+  a=num*num;
+  System.out.println("the squre of num is "+ a);
+	}
+
+}
