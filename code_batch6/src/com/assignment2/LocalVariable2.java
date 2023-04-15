2e824d9930103b2c57a4ead8471d30010c0cee2e
asus <asus@LAPTOP-LN2QCEDH>
2023-04-15 10:11:20 +0530

    local variable

---
+++ code_batch6/src/com/assignment2/LocalVariable2.java
@@ -0,0 +1,18 @@
+package com.assignment2;
+
+public class LocalVariable2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		short s=50;
+	
+		System.out.println(s);
+	}
+
+
+
+}
+	
+	
+
