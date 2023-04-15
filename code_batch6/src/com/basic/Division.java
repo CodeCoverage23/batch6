ff40b422cf2c76e99cb0c0d94dc99f30eb1358e8
asus <asus@LAPTOP-LN2QCEDH>
2023-04-15 07:44:21 +0530

    division program

--- code_batch6/src/com/basic/Division.java
+++ code_batch6/src/com/basic/Division.java
@@ -1,14 +1,7 @@
 package com.basic;
 
-public class Division {
+public class  {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-int a=24;
-int b=6;
-int c=a/b;
-System.out.println(c);
 
-	}
-
-}
