3087bc5794d78eb79302a621cb52fcd56a5d25b7
PC <PC@DESKTOP-JFV6KS0>
2023-04-15 10:10:05 +0530

    leap year update

---
+++ code_batch6/src/Assignment_5/LeapYear.java
@@ -0,0 +1,16 @@
+package Assignment_5;
+
+public class LeapYear {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int year = 2020;
+if(((year%4==0)&&(year%100!=0))||(year%400==0)) {
+	System.out.println("LEAP YEAR");
+}
+else {
+	System.out.println("COMMAN YEAR");
+}
+	}
+
+}
