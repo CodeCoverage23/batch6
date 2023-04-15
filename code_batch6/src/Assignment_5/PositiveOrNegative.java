5c70a89bc7781d18cf784b0b77119f910fcbe010
PC <PC@DESKTOP-JFV6KS0>
2023-04-15 09:42:59 +0530

    PositiveOrNegative

---
+++ code_batch6/src/Assignment_5/PositiveOrNegative.java
@@ -0,0 +1,20 @@
+package Assignment_5;
+
+public class PositiveOrNegative {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int num =21;
+if(num>0)
+{
+	System.out.println("Number is Positive");
+}else if(num<0)
+{
+	System.out.println("Number is Negative");
+}else
+{
+	System.out.print("Number is Zero");
+}
+	}
+
+}
