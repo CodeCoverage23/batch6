c5935456e47e851c94747d58cdd1e26bf22c8335
asus <asus@LAPTOP-LN2QCEDH>
2023-04-15 15:54:04 +0530

    Positive or Negative number Assignment 5

---
+++ code_batch6/src/com/Assignment5/CheckNumberIsPositiveOrNegative.java
@@ -0,0 +1,21 @@
+package com.Assignment5;
+
+public class CheckNumberIsPositiveOrNegative {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     int num=15;
+     if(num>0){
+     System.out.println("positive");
+     }else {
+    	 if(num<0) {
+    		 System.out.println("negative");
+    	 }
+    		 else {
+    	 }
+     
+     
+     
+	}
+	}
+}
