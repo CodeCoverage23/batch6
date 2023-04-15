b5f6a71141066642c59126eb5f7d9b049fc0fe80
asus <asus@LAPTOP-LN2QCEDH>
2023-04-15 07:42:34 +0530

    update

---
+++ code_batch6/src/com/basic/Table.java
@@ -0,0 +1,20 @@
+
+package com.basic;
+
+public class Table {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         int num=1;
+         for(int i=1;i<=10;i++);
+         {
+        	System.out.println();
+        	 for(int j=2;j<=10;j++);
+            {
+          
+         }
+         }
+	}
+}
+
+
