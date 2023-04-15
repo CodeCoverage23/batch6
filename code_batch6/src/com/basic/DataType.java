6560121f306aed6f42331ad325955f5b4492ac6a
PC <PC@DESKTOP-JFV6KS0>
2023-04-15 12:49:09 +0530

    DataType

---
+++ code_batch6/src/com/basic/DataType.java
@@ -0,0 +1,18 @@
+package com.basic;
+
+public class DataType {
+static boolean b;
+short s;
+int i;
+long l;
+float f;
+double d;
+char c;
+VariableDemo v;
+public static void main(String[] args) {
+	byte bt = 1;
+	System.out.print(b);
+	System.out.print(bt);
+}
+
+}
