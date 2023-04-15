d211e0f5bdc8016b94c1bea226e677e9b837e2db
asus <asus@LAPTOP-LN2QCEDH>
2023-04-15 15:30:19 +0530

    Premative data types

---
+++ code_batch6/src/com/assignment2/PremativeDataTypes.java
@@ -0,0 +1,20 @@
+package com.assignment2;
+
+public class PremativeDataTypes {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte b;
+		short s;
+		int i;
+		long l;
+		float f;
+		double d;
+		boolean a;
+		char ch;	
+		
+
+	}
+
+}
