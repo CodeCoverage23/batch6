c3ec2aa6a2eaeee49635007a324cce8e3e9bc975
GALAXY <GALAXY@DESKTOP-OBKU59L>
2023-04-16 20:09:04 +0530

    Average of five subject

---
+++ code_batch6/src/com/basic/Average.java
@@ -0,0 +1,38 @@
+package com.basic;
+
+import java.util.Scanner;
+
+import java.util.Scanner;
+
+public class Average {
+
+	public static void main(String[] args) {
+
+		Scanner s = new Scanner(System.in);
+
+		System.out.println("Enter array size: ");
+		int size = s.nextInt();
+
+		int[] array = new int[size];
+
+		System.out.println("Enter array values :  ");
+		for (int i = 0; i < size; i++) {
+			int value = s.nextInt();
+			array[i] = value;
+
+		}
+
+		int length = array.length;
+
+		int sum = 0;
+
+		for (int i = 0; i < array.length; i++) {
+			sum += array[i];
+		}
+
+		double average = sum / length;
+
+		System.out.println("Average of array : " + average);
+
+	}
+}
