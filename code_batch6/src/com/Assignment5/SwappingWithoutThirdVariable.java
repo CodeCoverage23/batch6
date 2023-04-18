4eeff497b4dd49b0715271d804444a78b94ec48d
asus <asus@LAPTOP-LN2QCEDH>
2023-04-18 16:37:49 +0530

    swapping without third variable

---
+++ code_batch6/src/com/Assignment5/SwappingWithoutThirdVariable.java
@@ -0,0 +1,22 @@
+package com.Assignment5;
+
+public class SwappingWithoutThirdVariable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int a=15;
+       int b=10;
+       System.out.println("value before swapping: "+a+" "+b);
+       a=a+b;
+       b=a-b;
+       a=a-b;
+       System.out.println("value after swapping: "+a+" "+b);
+		
+		
+		
+		
+		
+		
+	}
+
+}
