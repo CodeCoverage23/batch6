a084456363c8283489dba5c8cb026e1325456c7c
Hemraj <Hemraj@DESKTOP-U0HKUMH>
2023-04-19 21:59:23 +0530

    CountChar

---
+++ demo/src/com/Opps/CharString.java
@@ -0,0 +1,15 @@
+package com.Opps;
+
+public class CharString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          String name = "Code Coverage";
+          int cou = 0;
+          for(int i = 0; i<name.length();i++) {
+        	  cou++;
+          }
+          System.out.println(" count char"+cou);
+	}
+   
+}
