ab5ee79a69c22aaa9530e245d9fb07bb1b67f946
SurajDotNetDeveloper <surajcool9623@gmail.com>
2023-04-20 07:11:28 +0530

    String total characters count

---
+++ code_batch6/src/com/java/String_Char_Count.java
@@ -0,0 +1,15 @@
+package com.java;
+
+public class String_Char_Count {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String name="CodeCoverage";
+ int a=0;
+ for( int i=0; i<name.length();i++) {
+	 a++;
+ }
+ System.out.println("number of character in codecoverage is "+ a);
+	}
+
+}
