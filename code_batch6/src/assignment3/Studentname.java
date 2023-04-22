96c12f8f3860a953267f2f7659c963e795e860c4
vaibhav <vaibhav@DESKTOP-FOGA3O1>
2023-04-22 16:56:16 +0530

    assignment 3

---
+++ code_batch6/src/assignment3/Studentname.java
@@ -0,0 +1,17 @@
+package assignment3;
+
+public class Studentname {
+
+	public String getStudentname(String name) {
+		return name;
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		Studentname s=new Studentname();
+		System.out.println(s.getStudentname("vaishnavi"));
+		// TODO Auto-generated method stub
+
+	}
+
+}
