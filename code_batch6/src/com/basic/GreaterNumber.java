825d968ec653248e0e754f1db871a96734dc506d
asus <asus@LAPTOP-LN2QCEDH>
2023-05-02 19:27:44 +0530

    greater number.

---
+++ code_batch6/src/com/basic/GreaterNumber.java
@@ -0,0 +1,29 @@
+package com.basic;
+
+public class GreaterNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int a=10,b=20;
+		 if(a>b) {
+			 System.out.println("a is greater");
+		 } 
+		 else
+		 {
+			 System.out.println("b is greater");
+			 }
+			 }
+			 
+			 
+		 }
+		
+		
+		
+		
+		
+		
+		
+		
+	
+
+
