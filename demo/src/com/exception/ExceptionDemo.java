f13d4ef2834a3116fb15f1df00986b9eee794003
mansi <mansi@ASUS>
2023-05-03 16:00:55 +0530

    exception

---
+++ demo/src/com/exception/ExceptionDemo.java
@@ -0,0 +1,13 @@
+package com.exception;
+
+public class ExceptionDemo {
+	
+	public static void main(String[] args) {
+		int i = 10/0; //yha exception aane ke vajah se kuch bhi print nahi hoga
+		System.out.println("2222222222");
+		System.out.println("2222222222");
+		System.out.println(i);
+		System.out.println("2222222222");
+		System.out.println("2222222222");
+	}
+}
