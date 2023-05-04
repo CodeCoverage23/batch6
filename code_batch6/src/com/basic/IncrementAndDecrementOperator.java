8fc9891848b84b221415691b39412c7c5cd5a789
asus <asus@LAPTOP-LN2QCEDH>
2023-05-04 18:22:16 +0530

    increment and decrement

---
+++ code_batch6/src/com/basic/IncrementAndDecrementOperator.java
@@ -0,0 +1,20 @@
+package com.basic;
+
+public class IncrementAndDecrementOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=12;
+int b=16;
+//      System.out.println(a++);
+//     System.out.println(a);
+//     System.out.println(++a);
+	System.out.println(a++ + ++b);
+		System.out.println(a+b);
+		
+		
+		
+		
+	}
+
+}
