ef6a94f4fb58d9fb9ed2ff1bafcb5234dca36a23
asus <asus@LAPTOP-LN2QCEDH>
2023-05-04 18:23:59 +0530

    Relational 1.

---
+++ code_batch6/src/com/basic/Relational1.java
@@ -0,0 +1,16 @@
+package com.basic;
+
+public class Relational1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     int a=11;
+     int b=12;
+     System.out.println((a>b)?a:b);
+		
+		
+		
+		
+	}
+
+}
