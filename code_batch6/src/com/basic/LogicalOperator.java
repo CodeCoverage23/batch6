0288aa1b225874df642b05c8c081082d19c45bcd
asus <asus@LAPTOP-LN2QCEDH>
2023-05-04 18:22:55 +0530

    logical

---
+++ code_batch6/src/com/basic/LogicalOperator.java
@@ -0,0 +1,19 @@
+package com.basic;
+
+public class LogicalOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=10;
+int b=20;
+int c=30;
+System.out.println((a<++b)&&(++a>c));
+System.out.println(a);
+System.out.println(a<b&++a>b);		
+	System.out.println(a<b|++a>b);	
+		
+		System.out.println(a);
+		System.out.println(b);
+	}
+
+}
