df93758ee8c4054f8a128576008d0214d2049798
asus <asus@LAPTOP-LN2QCEDH>
2023-05-04 18:26:44 +0530

    Greater number 2

---
+++ code_batch6/src/com/basic/GreaterNumber2.java
@@ -0,0 +1,25 @@
+package com.basic;
+
+public class GreaterNumber2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int a=150,b=30,c=45;
+        if(a>b && a>c) {
+        	System.out.println("a is greater");
+        }
+        	else if(b>a && b>c) {
+        		System.out.println("b is greater");
+        	}
+        		else
+        		{
+        			System.out.println("c is greater");
+        		}
+	}
+        	
+}  
+		
+		
+		
+	
+
