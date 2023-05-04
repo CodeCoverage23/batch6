f7652791a1bb7fd0c66332e24cd4e63d8703455d
asus <asus@LAPTOP-LN2QCEDH>
2023-05-04 18:24:32 +0530

    update.

---
+++ code_batch6/src/com/basic/RelationalOperator.java
@@ -0,0 +1,19 @@
+package com.basic;
+
+public class RelationalOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=10;
+int b=10;
+System.out.println(a>b);
+System.out.println(a<b);
+System.out.println(a>=b);
+System.out.println(a<=b);
+System.out.println(a==b);
+System.out.println(a!=b);
+
+
+	}
+
+}
