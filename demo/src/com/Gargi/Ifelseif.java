580245de9461f5467bc3f14343b2cfbebec24f73
Hemraj <Hemraj@DESKTOP-U0HKUMH>
2023-06-19 12:14:25 +0530

    if

---
+++ demo/src/com/Gargi/Ifelseif.java
@@ -0,0 +1,20 @@
+package com.Gargi;
+
+public class Ifelseif {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     int a = 65;
+     if(a==22) {
+     System.out.println("a is qual to 22");
+	}else if(a<22) {
+		System.out.println("a is less than 22");
+	} else if(a>22) {
+		System.out.println("a is greater than 22");
+	}else {
+		System.out.println("not any one");
+	}
+
+}
+}
+
