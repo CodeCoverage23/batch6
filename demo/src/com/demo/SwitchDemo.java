16100c624428a065d7bc0852b0b9d53e2833b973
User <User@DESKTOP-OQ2KTH4>
2023-06-20 19:30:15 +0530

    ....

---
+++ demo/src/com/demo/SwitchDemo.java
@@ -0,0 +1,15 @@
+package com.demo;
+
+public class SwitchDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name="Pradip";
+		switch(name) {
+		case "Arpita":
+			System.out.println("Rainy");
+		}
+
+	}
+
+}
